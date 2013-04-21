f9964bab41a0027573da0d0a5484fc249116842e
Eric Crawford <e2crawfo@uwaterloo.ca>
2013-04-21 11:48:08 -0400

    Updated oscillator.py to comply with new api
    Notes:
    *currently assuming that functions passed to make_node take time as an
    input...this is subject to change over to Terry's generator scheme.

--- examples/oscillator.py
+++ examples/oscillator.py
@@ -1,18 +1,41 @@
-import nef.nef_theano as nef
+from .. import nengo as nengo
+from nengo.connection import gen_transform
 
-net=nef.Network('Oscillator') #Create the network object
+## This example demonstrates how to create a neuronal ensemble that acts as an oscillator
+##
+## Network diagram:
+##
+##      [Input (2D)] ---> (Neurons) --
+##                              ^     |
+##                              |     |
+##                              -------         
 
-net.make_input('input',[1,0],
-    zero_after_time=0.1) #Create a controllable input function with a 
-                         #starting value of 1 and zero, then make it go 
-                         #to zero after .1s
-                         
-net.make('A',200,2) #Make a population with 200 neurons, 2 dimensions
+# Create the nengo model
+model = nengo.Model('Oscillator')         # Create the network object
 
-net.connect('input','A')
-net.connect('A','A',[[1,1],[-1,1]],pstc=0.1) #Recurrently connect the population 
-                                         #with the connection matrix for a 
-                                         #simple harmonic oscillator mapped 
-                                         #to neurons with the NEF
+# Make controllable inputs
+def start_input(t):
+    if t < 0.1:
+        return [1,0]
+    else:
+        return [0,0]
 
-net.run(1) # run for 1 second
+model.make_node('Input', start_input)     # Create an input function that gets set to 
+                                          #   to zero after .1s
+
+# Create the neuronal ensembles
+model.make_ensemble('Neurons', 200, 2)    # Make a population with 200 neurons, 2 dimensions
+
+# Create the connections within the model
+model.connect('Input','Neurons')
+
+model.connect('Neurons','Neurons',        # Recurrently connect the population 
+              transform=[[1,1], [-1,1]],  #   with the connection matrix for a 
+              filter=ExponentialPSC(0.1)) #   simple harmonic oscillator mapped 
+                                          #   to neurons with the NEF
+
+# Build the model
+model.build()                             # Generate model parameters
+
+# Run the model
+model.run(1)                              # Run for 1 second
