43355fe950bcab07afea9dc84de1fc40911954b8
drasmuss <drasmuss@uwaterloo.ca>
2013-04-21 14:46:51 -0400

    Initial implementation of Connection

--- nengo/connection.py
+++ nengo/connection.py
@@ -1,21 +1,30 @@
-
+import numpy as np
 
 from .filter import Filter
 
 class Connection(object):
-    """A connection between two objects (Ensembles, Nodes, Networks)
-
-    This class describes a connection between two objects. It contains
-    the source (pre-population) and destination (post-population) of
-    the connection. It also contains information about the computation of
-    the connection, including functions or dimension transforms. Alternatively,
-    it can represent a direct neuron-to-neuron connection by passing in a
-    weight matrix. Finally, the connection can perform learning if given a
-    learning rule.
-    """
     
     def __init__(self, pre, post, 
                  transform=None, function=None, weights=None,
+                 filter=Filter(), learning_rule=None):
+        vector_space = transform is not None or function is not None
+        neuron_space = weights is not None
+        if vector_space and neuron_space:
+            raise ValueError(
+                "Cannot provide both vector_space arguments (transform and function)" + 
+                " and neuron space arguments (weights).")
+            
+        if vector_space:
+            return VectorConnection()
+        
+        if neuron_space:
+            return NeuronConnection()
+        
+
+class VectorConnection(Connection):
+    
+    def __init__(self, pre, post, 
+                 transform=None, function=None, 
                  filter=Filter(), learning_rule=None):
         """
         Create a new connection between two objects. This connection should
@@ -28,27 +37,16 @@ class Connection(object):
         :type transform: a (pre.dimensions x post.dimensions) array of floats
         :param function: the vector-space function to be computed by the 
             pre-population decoders
-        :param weights: the connection-weight matrix for connecting pre-neurons
-            to post-neurons directly. Cannot be used with transform or function.
-        :type weights: a (pre.neurons x post.neurons) array of floats
         :param filter: a Filter object describing the post-synaptic filtering
             properties of the connection
         :param learning_rule: a LearningRule object describing the learning
             rule to use with the population
         """
-        vector_space = transform is not None or function is not None
-        neuron_space = weights is not None
-        if vector_space and neuron_space:
-            raise ValueError(
-                "Cannot provide both vector_space arguments (transform and function)" + 
-                " and neuron space arguments (weights).")
-
         ### basic parameters, set by network.connect(...)
         self.pre = pre
         self.post = post
         self.transform = transform
         self.function = function
-        self.weights = weights
         self.filter = filter
         self.learning_rule = learning_rule
 
@@ -128,3 +126,15 @@ class Connection(object):
             transform = array_transform
 
         return transform
+    
+    def get_transformed_pre(self, state, dt):
+        """Returns the transformed, filtered value output from pre."""
+        
+        pre_in = state[self.pre]
+        pre_in = self.transform*pre_in
+        pre_in = self.filter.update(dt, source=pre_in)
+        return input
+
+class NeuronConnection:
+    def __init__(self):
+        pass
--- nengo/filter.py
+++ nengo/filter.py
@@ -3,9 +3,9 @@ import collections
 import numpy as np
 
 class Filter:
-    """Filter an arbitrary theano.shared"""
+    """Filter an arbitrary value"""
 
-    def __init__(self, pstc, name=None, source=None, shape=None):
+    def __init__(self, pstc, name=None, source=None, dimension=None):
         """
         :param float pstc:
         :param string name:
@@ -15,37 +15,26 @@ class Filter:
         """
         self.pstc = pstc
         self.source = source
-
-        ### try to find the shape from the given parameters (source and shape)
-        if source is not None and hasattr(source, 'get_value'):
-            value = source.get_value()
-            if shape is not None:
-                assert value.shape == shape
-            if name is None: 
-                name = 'filtered_%s' % source.name
-        elif shape is not None:
-            value = np.zeros(shape, dtype='float32')
+        if source == None:
+            if dimension == None:
+                raise Exception("Must pass a dimension to filter if no source given")
+            self.value = np.array([0.0 for i in range(dimension)])
         else:
-            raise Exception("Either \"source\" or \"shape\" must define filter shape")
-            
-        ### create shared variable to store the filtered value
-        self.value = theano.shared(value, name=name)
+            self.value = np.array([0.0 for i in range(source.shape[0])])
+        
 
-    def set_source(self, source):
-        """Set the source of data for this filter
-
-        :param source:
-        :type source:
-        """
-        self.source = source
-
-    def update(self, dt):
+    def filter(self, dt, source=None):
         """
         :param float dt: the timestep of the update
         """
+        if self.source:
+            source_input = self.source
+        else:
+            source_input = source
+        
         if self.pstc >= dt:
             decay = np.cast(np.exp(-dt / self.pstc), self.value.dtype)
-            value_new = decay * self.value + (1 - decay) * self.source
+            value_new = decay * self.value + (1 - decay) * source_input
             return collections.OrderedDict([(self.value, value_new.astype('float32'))])
         else:
             ### no filtering (pstc = 0), so just make the value the source
