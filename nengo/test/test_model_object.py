63b3a60795e9d15bd3a366b71191c2956d58f63d
James Bergstra <james.bergstra@gmail.com>
2013-04-29 16:58:04 -0400

    small comment in model_object tests

--- nengo/test/test_model_object.py
+++ nengo/test/test_model_object.py
@@ -2,6 +2,9 @@
 import test_model
 from nengo import model_object
 
+# TODO: import other unit tests that use model.Model
+#       and swap in model_object.Model
+
 class TestModelObject(test_model.TestModel):
     Model = model_object.Model
 
