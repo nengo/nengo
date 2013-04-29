49bb061dce3dba49fe92e53b4b4cdd29ec385f4f
James Bergstra <james.bergstra@gmail.com>
2013-04-29 16:57:38 -0400

    remove object_api_*

--- nengo/test/test_object_api_python.py
+++
@@ -1,7 +0,0 @@
-"""
-Test unique aspects of the python API implementation here.
-
-For general correctness of the API implementation, see test_object_api,
-which tests the python ("reference") backend.
-"""
-
