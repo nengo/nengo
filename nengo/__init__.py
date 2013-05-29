8395b663b261d0d6805aab9ba8c3bccc3874193a
James Bergstra <james.bergstra@gmail.com>
2013-05-29 09:27:39 -0400

    Removing all nengo python files
    Clearing them for rewrite based on code from nengo_ocl

--- nengo/__init__.py
+++ nengo/__init__.py
@@ -1,22 +1,2 @@
 __copyright__ = "2013, Nengo contributors"
 __license__ = "http://www.gnu.org/licenses/gpl.html"
-
-from .network import Network
-
-basic = [Network]
-
-from .ensemble import Ensemble
-from .input import Input
-from .simplenode import SimpleNode
-
-from .probe import Probe
-
-from .origin import Origin
-from .ensemble_origin import EnsembleOrigin
-
-from .learned_termination import LearnedTermination
-
-advanced = [Ensemble, Input, SimpleNode, Probe, Origin,
-            EnsembleOrigin, LearnedTermination]
-
-__all__ = basic + advanced
