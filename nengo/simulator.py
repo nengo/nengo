284bc678295f9f8bb9b1e5118456c9746a4a2426
James Bergstra <james.bergstra@gmail.com>
2013-05-31 14:09:53 -0400

    First draft of Signal indexing support

--- nengo/simulator.py
+++ nengo/simulator.py
@@ -8,6 +8,37 @@ import numpy as np
 from neuron import step_lif
 from neuron import step_lif_rate
 
+def get_signal(signals_dct, obj):
+    # look up a Signal or SignalView
+    # in a `signals_dct` such as self.signals
+    if obj in signals_dct:
+        return signals_dct[obj]
+    elif obj.base in signals_dct:
+        base_array = signals_dct[obj.base]
+        try:
+            # wtf?
+            itemsize = int(obj.dtype.itemsize)
+        except TypeError:
+            itemsize = int(obj.dtype().itemsize)
+        byteoffset = itemsize * obj.offset
+        bytestrides = [itemsize * s for s in obj.elemstrides]
+        view = np.ndarray(shape=obj.shape,
+                          dtype=obj.dtype,
+                          buffer=base_array.data,
+                          offset=byteoffset,
+                          strides=bytestrides,
+                         )
+        view[...]
+        return view
+    else:
+        raise TypeError()
+        
+
+def zero_array_dct(dct):
+    for arr in dct.values():
+        arr[...] = 0
+
+
 class Simulator(object):
     def __init__(self, model):
         self.model = model
@@ -39,28 +70,19 @@ class Simulator(object):
         for probe in self.model.signal_probes:
             self.probe_outputs[probe] = []
 
-    def get_signal(self, signals_dct, obj):
-        if obj in signals_dct:
-            return signals_dct
-        elif obj.base in signals_dct:
-            base = signals_dct[obj.base]
-            raise base.TODO('fetch view')
-        else:
-            raise TypeError()
-            
 
     def step(self):
         # population bias: bias -> input current
         for pop in self.model.populations:
-            self.populations[pop]['ic'] = 1.0 * self.populations[pop]['jbias']
+            self.populations[pop]['ic'][...] = self.populations[pop]['jbias']
 
         # encoders: signals -> input current
         for enc in self.model.encoders:
             self.populations[enc.pop]['ic'] += np.dot(
-                self.get_signal(self.signals,enc.sig),
+                get_signal(self.signals,enc.sig),
                 enc.weights.T)
 
-        # population dynamics
+        # population dynamics (TODO: registry)
         for pop in self.model.populations:
             step_lif(
                 self.populations[pop]['ic'],
@@ -73,6 +95,7 @@ class Simulator(object):
                 upsample=pop.upsample,
                 )
 
+        # population dynamics (TODO: registry)
         for pop in self.model.populations_lif_rate:
             step_lif_rate(
                 self.populations[pop]['ic'],
@@ -83,37 +106,47 @@ class Simulator(object):
                 )
 
         # decoders: population output -> signals_tmp
+        zero_array_dct(self.signals_tmp)
         for dec in self.model.decoders:
-            self.signals_tmp[dec.sig] = np.dot(
+            get_signal(self.signals_tmp, dec.sig)[...] += np.dot(
                 self.populations[dec.pop]['out'],
                 dec.weights.T)
 
         # -- copy: signals -> signals_copy
         for sig in self.model.signals:
-            self.signals_copy[sig] = 1.0 * self.signals[sig]
-
-        # -- reset: 0 -> signals
-        for sig in self.model.signals:
-            self.signals[sig][...] = 0
+            self.signals_copy[sig][...] = self.signals[sig]
 
         # -- filters: signals_copy -> signals
+        zero_array_dct(self.signals)
         for filt in self.model.filters:
             new, old = filt.newsig, filt.oldsig
-            self.signals[new] += filt.alpha * self.signals_copy[old]
+            inc =  np.dot(filt.alpha, get_signal(self.signals_copy, old))
+            targ = get_signal(self.signals, new)
+            # -- we check for size mismatch,
+            #    because incrementing scalar to len-1 arrays is ok
+            #    if the shapes are not compatible, we'll get a 
+            #    problem in targ[...] += inc
+            if inc.size != targ.size:
+                raise ValueError('shape mismatch in filter',
+                                 (filt, inc.shape, targ.shape))
+            targ[...] += inc
 
         # -- transforms: signals_tmp -> signals
         for tf in self.model.transforms:
-            self.signals[tf.outsig] += tf.alpha * self.signals_tmp[tf.insig]
+            get_signal(self.signals, tf.outsig)[...] += np.dot(
+                tf.alpha,
+                get_signal(self.signals_tmp, tf.insig))
 
         # -- customs: signals -> signals
         for ct in self.model.custom_transforms:
-            self.signals[ct.outsig][...] = ct.func(self.signals[ct.insig])
+            get_signal(self.signals, ct.outsig)[...] = ct.func(
+                get_signal(self.signals, ct.insig))
 
         # -- probes signals -> probe buffers
         for probe in self.model.signal_probes:
             period = int(probe.dt / self.model.dt)
             if self.n_steps % period == 0:
-                tmp = self.signals[probe.sig].copy()
+                tmp = get_signal(self.signals, probe.sig).copy()
                 self.probe_outputs[probe].append(tmp)
 
         self.n_steps += 1
