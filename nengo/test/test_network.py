da5c7ec07a86c6e790edf1325340e6200945e58c
James Bergstra <james.bergstra@gmail.com>
2013-06-01 12:14:26 -0400

    Cleanup test dir
    Running nosetests in the project should now
    * complete successfully
    * pass >= three tests
    * skip the rest of them.

--- nengo/test/test_network.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from nengo.model import Model
-from nengo.nef.network import Network
-
-class TestNetwork(TestCase):
-    def setUp(self):
-        model = Model("Test Model")
-    
