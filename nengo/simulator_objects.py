e8e82faf004c792a3378eca3023b09d1ba221265
James Bergstra <james.bergstra@gmail.com>
2013-06-05 17:15:19 -0400

    Added signals for alpha and weights
    Encoder, Decoder, Transform and Filter coefficients have shapes, can
    potentially change over time (plasticity / adaptation / learning), and
    are indexed the same way as other signals in the OpenCL codebase.  This
    change standardizes the handling of numbers within the simulator by
    making all these constants into signals.

--- nengo/simulator_objects.py
+++ nengo/simulator_objects.py
@@ -170,17 +170,31 @@ class Nonlinearity(object):
 class Transform(object):
     """A linear transform from a decoded signal to the signals buffer"""
     def __init__(self, alpha, insig, outsig):
-        self.alpha = alpha
+        alpha = np.asarray(alpha)
+        if hasattr(outsig, 'value'):
+            raise TypeError('transform destination is constant')
+        self.alpha_signal = Constant(n=alpha.size, value=alpha)
         self.insig = insig
         self.outsig = outsig
+
+    @property
+    def alpha(self):
+        return self.alpha_signal.value
+
+    @alpha.setter
+    def alpha(self, value):
+        self.alpha_signal.value[...] = value
 
 
 class Filter(object):
     """A linear transform from signals[t-1] to signals[t]"""
     def __init__(self, alpha, oldsig, newsig):
+        if hasattr(newsig, 'value'):
+            raise TypeError('filter destination is constant')
+        alpha = np.asarray(alpha)
+        self.alpha_signal = Constant(n=alpha.size, value=alpha)
         self.oldsig = oldsig
         self.newsig = newsig
-        self.alpha = alpha
 
     def __str__(self):
         return '%s{%s, %s, %s}' % (
@@ -190,6 +204,13 @@ class Filter(object):
     def __repr__(self):
         return str(self)
 
+    @property
+    def alpha(self):
+        return self.alpha_signal.value
+
+    @alpha.setter
+    def alpha(self, value):
+        self.alpha_signal.value[...] = value
 
 class Encoder(object):
     """A linear transform from a signal to a population"""
@@ -202,8 +223,15 @@ class Encoder(object):
             weights = np.asarray(weights)
             if weights.shape != (pop.n_in, sig.size):
                 raise ValueError('weight shape', weights.shape)
-        self.weights = weights
+        self.weights_signal = Constant(n=weights.size, value=weights)
 
+    @property
+    def weights(self):
+        return self.weights_signal.value
+
+    @weights.setter
+    def weights(self, value):
+        self.weights_signal.value[...] = value
 
 class Decoder(object):
     """A linear transform from a population to a signal"""
@@ -216,8 +244,16 @@ class Decoder(object):
             weights = np.asarray(weights)
             if weights.shape != (sig.size, pop.n_out):
                 raise ValueError('weight shape', weights.shape)
-        self.weights = weights
-        
+        self.weights_signal = Constant(n=weights.size, value=weights)
+
+    @property
+    def weights(self):
+        return self.weights_signal.value
+
+    @weights.setter
+    def weights(self, value):
+        self.weights_signal.value[...] = value
+
 
 class SimModel(object):
     """
@@ -264,12 +300,16 @@ class SimModel(object):
         """Add an encoder to the model"""
         rval = Encoder(sig, pop, weights=weights)
         self.encoders.append(rval)
+        if rval.weights_signal not in self.signals:
+            self.signals.append(rval.weights_signal)
         return rval
 
     def decoder(self, pop, sig, weights=None):
         """Add a decoder to the model"""
         rval = Decoder(pop, sig, weights=weights)
         self.decoders.append(rval)
+        if rval.weights_signal not in self.signals:
+            self.signals.append(rval.weights_signal)
         return rval
 
     def neuron_connection(self, src, dst, weights=None):
@@ -279,16 +319,16 @@ class SimModel(object):
 
     def transform(self, alpha, insig, outsig):
         """Add a transform to the model"""
-        if hasattr(outsig, 'value'):
-            raise TypeError('transform destination is constant')
         rval = Transform(alpha, insig, outsig)
+        if rval.alpha_signal not in self.signals:
+            self.signals.append(rval.alpha_signal)
         self.transforms.append(rval)
         return rval
 
     def filter(self, alpha, oldsig, newsig):
         """Add a filter to the model"""
-        if hasattr(newsig, 'value'):
-            raise TypeError('filter destination is constant')
         rval = Filter(alpha, oldsig, newsig)
+        if rval.alpha_signal not in self.signals:
+            self.signals.append(rval.alpha_signal)
         self.filters.append(rval)
         return rval
