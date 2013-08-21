726e1a3f6e66ce294ddfa1adc2cdeba11114403d
hunse <hunse@ctn>
2013-08-21 11:24:24 -0400

    Fixed old_api.connect so that inputs can be filtered. - The old_api/integrator example now works   (Nengo-1.4 results reproduced)

--- examples/old_api/integrator.py
+++ examples/old_api/integrator.py
@@ -23,76 +23,60 @@ Network behaviour:
 
 """
 
-# from .. import nengo as nengo
-# from ..nengo.connection import gen_transfrom
-# from ..nengo.filter import ExponentialPSC
-
-import collections
-
 import nengo.old_api as nengo
 
-# Define model parameters
+### Define model parameters
 tau = 0.1
 
-# Create the nengo model
+### Create the nengo model
 model = nengo.Network('Integrator')
 
-# Create the model inputs
-input_d = {0.2:5, 0.3:0, 0.44:-10, 0.54:0, 0.8:5, 0.9:0}
+### Create the model inputs
+input_d = {0.2:5, 0.3:0, 0.44:-10, 0.54:0, 0.8:5, 0.9:0}.items()
+input_d.sort(reverse=True)
 def input_f(t):
-    # for key, value in input_d.items():
-    #     if t < key:
-    #         return value
-    # return 0.0
-    # t = t/10
-    # if t < 0.2: return 5
-    # elif t < 0.3: return 0
-    # elif t < 0.44: return -10
-    # elif t < 0.54: return 0
-    # elif t < 0.8: return 5
-    # elif t < 0.9: return 0
-    if t < 0.7:
-        return 1
-    else:
-        return 0
+    for key, value in input_d:
+        if t > key: return value
+    return 0.0
 
 model.make_input('Input', input_f)
 # model.make_input('Input', input_d)
 
-# Create the neuronal ensembles
-model.make('A', 100, 1)
+### Create the neuronal ensembles
+model.make('A', 100, 1, radius=1)
 
-# Create the connections within the model
-model.connect('Input', 'A', transform=tau, pstc=0.1)
-
+### Create the connections within the model
+# model.connect('Input', 'A', transform=[tau], pstc=0.005)
+model.connect('Input', 'A', transform=[tau], pstc=tau)
 model.connect('A', 'A', pstc=tau)
 
-# Add probes
-dt_sample = 0.01
-in_p = model.make_probe('Input', dt_sample, dt_sample)
-out_p = model.make_probe('A', dt_sample, dt_sample)
+### Add probes
+probe_dt = 0.01
+probe_tau = 0.03
+input_p = model.make_probe('Input', 0.001, 0.001)
+output_p = model.make_probe('A', probe_dt, probe_tau)
 
-# model.connect('Input', 'in_p')
-# model.connect('A', 'out_p')
-
-# Build the model
+### Build the model
 # model.build()
 
-# Run the model
-model.run(3)
+### Run the model
+t_final = 1.2
+model.run(t_final)
 
-# print in_p
+### Plot the results
+try:
+    import numpy as np
+    import matplotlib.pyplot as plt
+    plt.figure(1)
+    plt.clf()
+    x = input_p.get_data()
+    y = output_p.get_data()
+    t = lambda x: (t_final/len(x))*np.arange(len(x))
 
-# import ipdb
-# ipdb.set_trace()
-
-import numpy as np
-import matplotlib.pyplot as plt
-plt.figure(1)
-plt.clf()
-x = in_p.get_data()
-y = out_p.get_data()
-t = dt_sample*np.arange(len(x))
-plt.plot(t, x)
-plt.plot(t, y)
-plt.show()
+    plt.subplot(211)
+    plt.plot(t(x), x)
+    plt.subplot(212)
+    plt.plot(t(y), y)
+    plt.show()
+except ImportError:
+    print "Could not import required libraries for plotting"
