f32231119c25da0cc15fcb6d4f81ecc9bceea8d1
James Bergstra <james.bergstra@gmail.com>
2013-08-22 17:42:19 -0400

    try/except logging fallback for older Python
    Python2.6 evidentally has no NullHandler, so we just go on
    without it.

--- nengo/__init__.py
+++ nengo/__init__.py
@@ -1,6 +1,10 @@
 import logging
 log = logging.getLogger(__name__)
-log.addHandler(logging.NullHandler())  # Prevent output if no handler set
+try:
+    log.addHandler(logging.NullHandler())  # Prevent output if no handler set
+except AttributeError:
+    # -- older Python's don't have NullHandler.
+    pass
 
 from .model import Model
 from .objects import LIF
