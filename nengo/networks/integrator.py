123a50affaf8b887579da41ced312d75aec79e7e
drasmuss <drasmuss@uwaterloo.ca>
2013-09-12 16:24:53 -0400

    Integrator working with PassthroughNode

--- nengo/networks/integrator.py
+++ nengo/networks/integrator.py
@@ -10,7 +10,7 @@ class Integrator(Network):
         return self.node.signal
 
     def make(self, recurrent_tau, **ens_args):
-        self.node = self.add(objects.Node('In', output=lambda x: x))
+        self.node = self.add(objects.PassthroughNode("In"))
         self.ensemble = self.add(objects.Ensemble('Integrator', **ens_args))
         self.ensemble.connect_to(self.ensemble, filter=recurrent_tau)
         self.node.connect_to(self.ensemble, transform=recurrent_tau)
