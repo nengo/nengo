9ebf7db5e7e07d0c9d43b1a97ab3f1871b1ddc34
drasmuss <drasmuss@uwaterloo.ca>
2013-09-11 21:57:56 -0400

    Cleaning up comments

--- nengo/tests/test_simulator.py
+++ nengo/tests/test_simulator.py
@@ -17,9 +17,6 @@ class TestSimulator(SimulatorTestCase):
         two = m.add(Signal(n=2, name='b'))
         three = m.add(Signal(n=3, name='c'))
 
-#        m.add(Filter(1, three[0:1], one))
-#        m.add(Filter(2.0, three[1:], two))
-#        m.add(Filter([[0, 0, 1], [0, 1, 0], [1, 0, 0]], three, three))
         m._operators += [simulator.ProdUpdate(core.Constant(1), three[0:1], core.Constant(0), one)]
         m._operators += [simulator.ProdUpdate(core.Constant(2.0), three[1:], core.Constant(0), two)]
         m._operators += [simulator.DotInc(core.Constant([[0,0,1], [0,1,0], [1,0,0]]), three, m._get_output_view(three))]
@@ -41,13 +38,9 @@ class TestSimulator(SimulatorTestCase):
         sig = m.add(Signal())
 
         pop = m.add(Direct(n_in=1, n_out=1, fn=np.sin))
-#        m.add(Encoder(m.t, pop, weights=[[1.0]]))
-#        m.add(Decoder(pop, sig, weights=[[1.0]]))
-#        m.add(Transform(1.0, sig, sig))
         
         m._operators += [simulator.DotInc(core.Constant([[1.0]]), m.t, pop.input_signal)]
         m._operators += [simulator.ProdUpdate(core.Constant([[1.0]]), pop.output_signal, core.Constant(0), sig)]
-#        m._operators += [simulator.ProdUpdate(
 
         sim = m.simulator(sim_class=self.Simulator)
         sim.step()
@@ -64,10 +57,7 @@ class TestSimulator(SimulatorTestCase):
         m = nengo.Model("")
         foo = m.add(Signal(n=1, name='foo'))
         pop = m.add(Direct(n_in=2, n_out=2, fn=lambda x: x + 1, name='pop'))
-#        enc = m.add(Encoder(foo, pop, [[1.0], [2.0]]))
-#        dec = m.add(Decoder(pop, foo, [[.2, .1]]))
-#        tf = m.add(Transform(0.5, foo, foo))
-#        fi = m.add(Filter(0.2, foo, foo))
+        
         decoders = np.asarray([.2,.1])
         m._operators += [simulator.DotInc(Constant([[1.0],[2.0]]), foo, pop.input_signal)]
         m._operators += [simulator.ProdUpdate(Constant(decoders*0.5), pop.output_signal, Constant(0.2), foo)]
@@ -81,8 +71,6 @@ class TestSimulator(SimulatorTestCase):
                             "%s: value %s is not close to target %s" %
                             (sig, sim.signals[sim.copied(sig)], target))
         check(foo, .55)
-#        check(enc.sig, .55) # -- was 1.0 during step fn
-#        check(enc.weights_signal, [[1], [2]]) #
         try:
             check(pop.input_signal, [1, 2])
             check(pop.output_signal, [2, 3])
@@ -91,7 +79,6 @@ class TestSimulator(SimulatorTestCase):
             #    input signals are zerod
             check(pop.input_signal, [0, 0])
             check(pop.output_signal, [0, 0])
-#        check(sim.dec_outputs[dec.sig], [.7])
 
 
         self.assertTrue(np.allclose(sim.signals[sim.copied(foo)],
@@ -102,10 +89,7 @@ class TestSimulator(SimulatorTestCase):
         m = nengo.Model("")
         foo = m.add(Signal(n=1, name='foo'))
         pop = m.add(Direct(n_in=2, n_out=2, fn=lambda x: x + 1, name='pop'))
-#        enc = m.add(Encoder(foo[:], pop, [[1.0], [2.0]]))
-#        dec = m.add(Decoder(pop, foo, [[.2, .1]]))
-#        tf = m.add(Transform(0.5, foo, foo[:]))
-#        fi = m.add(Filter(0.2, foo[:], foo))
+
         decoders = np.asarray([.2,.1])
         m._operators += [simulator.DotInc(Constant([[1.0],[2.0]]), foo[:], pop.input_signal)]
         m._operators += [simulator.ProdUpdate(Constant(decoders*0.5), pop.output_signal, Constant(0.2), foo[:])]
@@ -119,11 +103,8 @@ class TestSimulator(SimulatorTestCase):
                             "%s: value %s is not close to target %s" %
                             (sig, sim.signals[sim.copied(sig)], target))
         check(foo, .55)
-#        check(enc.sig, .55) # -- was 1.0 during step fn
-#        check(enc.weights_signal, [[1], [2]]) #
         check(pop.input_signal, [1, 2])
         check(pop.output_signal, [2, 3])
-        # check(sim.dec_outputs[dec.sig], [.7])
 
         self.assertTrue(np.allclose(sim.signals[sim.copied(foo)],
                                     .55, atol=.01, rtol=.01),
