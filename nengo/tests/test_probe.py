49ce607a630428c15bc77d75056a66f44ec1aba8
hunse <hunse@ctn>
2013-09-19 16:27:25 -0400

    Added a new probe test

--- nengo/tests/test_probe.py
+++ nengo/tests/test_probe.py
@@ -1,6 +1,11 @@
+
+import numpy as np
+
 import nengo
 from nengo.tests.helpers import SimulatorTestCase, unittest
 
+import logging
+logger = logging.getLogger(__name__)
 
 class TestProbe(SimulatorTestCase):
 
@@ -18,6 +23,31 @@ class TestProbe(SimulatorTestCase):
         self.assertIsNotNone(sim.data(
             "This is an extremely long name that will test "
             "if we can access sim data with long names"))
+
+    def test_multirun(self):
+        """Test probing the time on multiple runs"""
+        rng = np.random.RandomState(2239)
+
+        rtol = 1e-4 # a bit higher, since model.t accumulates error over time
+
+        model = nengo.Model("Multi-run")
+        sim = model.simulator(sim_class=self.Simulator)
+        dt = sim.model.dt
+
+        # t_stops = [0.123, 0.283, 0.821, 0.921]
+        t_stops = dt * rng.randint(low=100, high=2000, size=10)
+
+        t_sum = 0
+        for ti in t_stops:
+            sim.run(ti)
+            sim_t = sim.data(model.t).flatten()
+            t = dt * np.arange(1, len(sim_t)+1)
+            self.assertTrue(np.allclose(sim_t, t, rtol=rtol))
+            # assert_allclose(self, logger, sim_t, t, rtol=rtol)
+
+            t_sum += ti
+            print t_sum, sim_t[-1]
+            self.assertTrue(np.allclose(sim_t[-1], t_sum, rtol=rtol))
 
 
 if __name__ == "__main__":
