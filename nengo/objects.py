f17f3067b4cd8c1082fc8c805590ec4428269a2d
drasmuss <drasmuss@uwaterloo.ca>
2013-12-11 13:41:13 -0500

    Removed has_input logic

--- nengo/objects.py
+++ nengo/objects.py
@@ -218,7 +218,6 @@ class Node(object):
         self.output = output
         self.label = label
         self.dimensions = dimensions
-        self.has_input = False
 
         # Set up probes
         self.probes = {'output': []}
@@ -298,11 +297,6 @@ class Connection(object):
 
         #add self to current context
         nengo.context.add_to_current(self)
-        if hasattr(post, 'has_input'):
-            #TODO: this is just used to detect whether the node
-            #needs time as input; remove this once we have
-            #a better way of indicating that
-            post.has_input = True
 
     def __str__(self):
         return self.label + " (" + self.__class__.__name__ + ")"
