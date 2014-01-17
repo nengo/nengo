1185d586ef214a0396b7c1bb8b0ed2e658586616
Eric Hunsberger <erichuns@gmail.com>
2014-01-17 17:23:56 -0500

    Added some more decoder methods.
    - Added some L1/sparse solvers
    - Decoder methods that solve for a weight matrix are now supported
    - Added tests
    - Added allclose function, allows you to account for delays,
      exclude the beginning and end of signal, and do a plot.
    - Added filtfilt function for delay-free filtering.
    - Also added some documentation related to decoders.

--- nengo/decoders.py
+++ nengo/decoders.py
@@ -1,50 +1,176 @@
 """
-This file contains functions concerned with solving for decoders
-or other types of weight matrices.
+Functions concerned with solving for decoders or full weight matrices.
 
-The idea is that as we develop new methods to find decoders either more
-quickly or with different constraints (e.g., L1-norm regularization),
-the associated functions will be placed here.
+Many of the solvers in this file can solve for decoders or weight matrices,
+depending on whether the post-population encoders `E` are provided (see below).
+Solvers that are only intended to solve for either decoders or weights can
+remove the `E` parameter or make it manditory as they see fit.
+
+All solvers take following arguments:
+  A : array_like (M, N)
+    Matrix of the N neurons' activities at the M evaluation points
+  Y : array_like (M, D)
+    Matrix of the target decoded values for each of the D dimensions,
+    at each of the M evaluation points.
+
+All solvers have the following optional keyword parameters:
+  rng : numpy.RandomState
+    A random number generator to use as required. If none is provided,
+    numpy.random will be used.
+  E : array_like (D, N2)
+    Array of post-population encoders. Providing this tells the solver
+    to return an array of connection weights rather than decoders.
+
+All solvers return the following:
+  X : np.ndarray (N, D) or (N, N2)
+    (N, D) array of decoders if E is none, or (N, N2) array of weights
+    if E is not none.
 """
 
 import numpy as np
+try:
+    import scipy.linalg
+    import scipy.optimize
+except ImportError:
+    scipy = None
 
-DEFAULT_RCOND = 0.01
+try:
+    import sklearn.linear_model
+except ImportError:
+    sklearn = None
 
 
-def lstsq_L2(activities, targets, rng, noise_amp=0.1):
+def lstsq(A, Y, rng=np.random, E=None, rcond=0.01):
+    """Unregularized least-squares."""
+    Y = np.dot(Y, E) if E is not None else Y
+    X, res, rank, s = np.linalg.lstsq(A, Y, rcond=rcond)
+    return X
+
+
+def lstsq_noise(A, Y, rng=np.random, E=None, noise_amp=0.1):
+    """Least-squares with additive white noise."""
+    sigma = noise_amp * A.max()
+    A = A + rng.normal(scale=sigma, size=A.shape)
+    Y = np.dot(Y, E) if E is not None else Y
+    return _cholesky(A, Y, 0)
+
+
+def lstsq_multnoise(A, Y, rng=np.random, E=None, noise_amp=0.1):
+    """Least-squares with multiplicative white noise."""
+    A = A + rng.normal(scale=noise_amp, size=A.shape) * A
+    Y = np.dot(Y, E) if E is not None else Y
+    return _cholesky(A, Y, 0)
+
+
+def lstsq_L2(A, Y, rng=np.random, E=None, noise_amp=0.1):
     """Least-squares with L2 regularization."""
-    sigma = noise_amp * activities.max()
-    return _cholesky1(activities, targets, sigma)
+    Y = np.dot(Y, E) if E is not None else Y
+    sigma = noise_amp * A.max()
+    return _cholesky(A, Y, sigma)
 
 
-def lstsq_L2nz(activities, targets, rng, noise_amp=0.1):
+def lstsq_L2nz(A, Y, rng, E=None, noise_amp=0.1):
     """Least-squares with L2 regularization on non-zero components."""
+    Y = np.dot(Y, E) if E is not None else Y
 
     # Compute the equivalent noise standard deviation. This equals the
     # base amplitude (noise_amp times the overall max activation) times
     # the square-root of the fraction of non-zero components.
-    sigma = (noise_amp * activities.max()) * np.sqrt((activities > 0).mean(0))
+    m, n = A.shape
+    sigma = (noise_amp * A.max()) * np.sqrt(
+        (A > 0).mean(axis=1 if m < n else 0))
 
     # sigma == 0 means the neuron is never active, so won't be used, but
     # we have to make sigma != 0 for numeric reasons.
     sigma[sigma == 0] = 1
 
     # Solve the LS problem using the Cholesky decomposition
-    return _cholesky1(activities, targets, sigma)
+    return _cholesky(A, Y, sigma)
 
 
-def _cholesky1(A, b, sigma):
-    """Solve the given linear system(s) using the Cholesky decomposition."""
-    reglambda = sigma ** 2 * A.shape[0]    # regularization parameter lambda
+def lstsq_L1(A, Y, rng, E=None, l1=1e-4, l2=1e-6):
+    """Least-squares with L1 and L2 regularization (elastic net).
 
-    G = np.dot(A.T, A)
+    This method is well suited for creating sparse decoders or weight matrices.
+    """
+    if sklearn is None:
+        raise RuntimeError(
+            "'lstsq_L1' requires the 'sklearn' package to be installed")
+
+    # TODO: play around with these regularization constants (I just guessed).
+    #   Do we need to scale regularization by number of neurons, to get same
+    #   level of sparsity? esp. with weights? Currently, setting l1=1e-3 works
+    #   well with weights when connecting 1D populations with 100 neurons each.
+    a = l1 * A.max()      # L1 regularization
+    b = l2 * A.max()**2   # L2 regularization
+    alpha = a + b
+    l1_ratio = a / (a + b)
+
+    ### solve least-squares A * X = Y
+    if E is not None:
+        Y = np.dot(Y, E)
+
+    model = sklearn.linear_model.ElasticNet(
+        alpha=alpha, l1_ratio=l1_ratio, fit_intercept=False, max_iter=1000)
+    model.fit(A, Y)
+    X = model.coef_.T
+    return X
+
+
+def lstsq_drop(A, Y, rng, E=None, noise_amp=0.1, drop=0.25, solver=lstsq_L2nz):
+    """Find sparser decoders/weights by dropping small values.
+
+    This solver first solves for coefficients (decoders/weights) with
+    L2 regularization, drops those nearest to zero, and retrains remaining.
+    """
+
+    # solve for coefficients using standard solver
+    X = solver(A, Y, rng=rng, noise_amp=noise_amp)
+    if E is not None:
+        X = np.dot(X, E)
+
+    # drop weights close to zero, based on `drop` ratio
+    Xabs = np.sort(np.abs(X.flat))
+    threshold = Xabs[int(np.round(drop * Xabs.size))]
+    X[np.abs(X) < threshold] = 0
+
+    # retrain nonzero weights
+    if E is not None:
+        Y = np.dot(Y, E)
+
+    for i in xrange(X.shape[1]):
+        nonzero = X[:, i] != 0
+        if nonzero.sum() > 0:
+            X[nonzero, i] = solver(A[:, nonzero], Y[:, i],
+                                   rng=rng, noise_amp=0.1 * noise_amp)
+
+    return X
+
+
+def _cholesky(A, b, sigma, transpose=None):
+    """
+    Find the least-squares solution of the given linear system(s)
+    using the Cholesky decomposition.
+    """
+    m, n = A.shape
+    transpose = m < n if transpose is None else transpose
+    if transpose:
+        # substitution: x = A'*xbar, G*xbar = b where G = A*A' + lambda*I
+        G = np.dot(A, A.T)
+    else:
+        # multiplication by A': G*x = A'*b where G = A'*A + lambda*I
+        G = np.dot(A.T, A)
+        b = np.dot(A.T, b)
+
+    reglambda = sigma ** 2 * m  # regularization parameter lambda
     np.fill_diagonal(G, G.diagonal() + reglambda)
-    b = np.dot(A.T, b)
 
-    L = np.linalg.cholesky(G)
-    L = np.linalg.inv(L.T)
-    return np.dot(L, np.dot(L.T, b))
+    if scipy is not None:
+        factor = scipy.linalg.cho_factor(G, overwrite_a=True)
+        x = scipy.linalg.cho_solve(factor, b)
+    else:
+        L = np.linalg.cholesky(G)
+        L = np.linalg.inv(L.T)
+        x = np.dot(L, np.dot(L.T, b))
 
-    # factor = sp.linalg.cho_factor(G, overwrite_a=True, check_finite=False)
-    # return sp.linalg.cho_solve(factor, b, check_finite=False)
+    return np.dot(A.T, x) if transpose else x
---
+++ nengo/tests/test_decoders.py
@@ -0,0 +1,195 @@
+"""
+TODO:
+  - add a test to test each solver many times on different populations,
+    and record the error.
+"""
+
+import logging
+
+import numpy as np
+import pytest
+
+import nengo
+from nengo.utils.distributions import UniformHypersphere
+from nengo.utils.functions import filtfilt
+from nengo.utils.numpy import rms
+from nengo.utils.testing import Plotter, allclose
+from nengo.decoders import (_cholesky, lstsq, lstsq_noise, lstsq_L2,
+                            lstsq_L2nz, lstsq_L1, lstsq_drop)
+
+logger = logging.getLogger(__name__)
+
+
+def sample_hypersphere(dimensions, n_samples, rng=np.random, surface=False):
+    return UniformHypersphere(
+        dimensions, surface=surface).sample(n_samples, rng=rng)
+
+
+def test_cholesky(Simulator):
+    rng = np.random.RandomState(4829)
+
+    m, n = 100, 100
+    A = rng.normal(size=(m, n))
+    b = rng.normal(size=(m, ))
+
+    x0, _, _, _ = np.linalg.lstsq(A, b)
+    x1 = _cholesky(A, b, 0, transpose=False)
+    x2 = _cholesky(A, b, 0, transpose=True)
+
+    assert np.allclose(x0, x1)
+    assert np.allclose(x0, x2)
+
+
+def test_weights(Simulator):
+    solver = lstsq_L2
+    rng = np.random.RandomState(39408)
+
+    d = 2
+    m, n = 100, 101
+    n_samples = 1000
+
+    a = nengo.LIF(m)  # population, for generating LIF tuning curves
+    a.set_gain_bias(rng.uniform(50, 100, m), rng.uniform(-1, 1, m))
+
+    ea = sample_hypersphere(d, m, rng=rng, surface=True).T  # pre encoders
+    eb = sample_hypersphere(d, n, rng=rng, surface=True).T  # post encoders
+
+    p = sample_hypersphere(d, n_samples, rng=rng)  # training eval points
+    A = a.rates(np.dot(p, ea))                     # training activations
+    X = p                                          # training targets
+
+    # find decoders and multiply by encoders to get weights
+    D = solver(A, X, rng=rng)
+    W1 = np.dot(D, eb)
+
+    # find weights directly
+    W2 = solver(A, X, rng=rng, E=eb)
+
+    # assert that post inputs are close on test points
+    pt = sample_hypersphere(d, n_samples, rng=rng)  # testing eval points
+    At = a.rates(np.dot(pt, ea))                    # testing activations
+    Y1 = np.dot(At, W1)                             # post inputs from decoders
+    Y2 = np.dot(At, W2)                             # post inputs from weights
+    assert np.allclose(Y1, Y2)
+
+    # assert that weights themselves are close (this is true for L2 weights)
+    assert np.allclose(W1, W2)
+
+
+@pytest.mark.benchmark
+def test_solvers(Simulator, nl_nodirect):
+
+    N = 100
+    decoder_solvers = [lstsq, lstsq_noise, lstsq_L2, lstsq_L2nz, lstsq_L1]
+    weight_solvers = [lstsq_L1, lstsq_drop]
+
+    dt = 1e-3
+    tfinal = 4
+
+    def input_function(t):
+        return np.interp(t, [1, 3], [-1, 1], left=-1, right=1)
+
+    model = nengo.Model('test_solvers', seed=290)
+    u = nengo.Node(output=input_function)
+    # up = nengo.Probe(u)
+
+    a = nengo.Ensemble(nl_nodirect(N), dimensions=1)
+    ap = nengo.Probe(a)
+    nengo.Connection(u, a)
+
+    probes = []
+    names = []
+    for solver, arg in ([(s, 'decoder_solver') for s in decoder_solvers] +
+                        [(s, 'weight_solver') for s in weight_solvers]):
+        b = nengo.Ensemble(nl_nodirect(N), dimensions=1, seed=99)
+        nengo.Connection(a, b, **{arg: solver})
+        probes.append(nengo.Probe(b))
+        names.append(solver.__name__ + (" (%s)" % arg[0]))
+
+    sim = nengo.Simulator(model, dt=dt)
+    sim.run(tfinal)
+    t = sim.trange()
+
+    # ref = sim.data(up)
+    ref = filtfilt(sim.data(ap), 20)
+    outputs = np.array([sim.data(probe) for probe in probes])
+    outputs_f = filtfilt(outputs, 20, axis=1)
+
+    close = allclose(t, ref, outputs_f,
+                     plotter=Plotter(Simulator, nl_nodirect),
+                     filename='test_decoders.test_solvers.pdf',
+                     labels=names,
+                     atol=0.05, rtol=0, buf=100, delay=7)
+    for name, c in zip(names, close):
+        assert c, "Solver '%s' does not meet tolerances" % name
+
+
+@pytest.mark.benchmark  # noqa: C901
+def test_regularization(Simulator, nl_nodirect):
+
+    ### TODO: multiple trials per parameter set, with different seeds
+
+    solvers = [lstsq_L2, lstsq_L2nz]
+    neurons = np.array([10, 20, 50, 100])
+    regs = np.linspace(0.01, 0.3, 16)
+    filters = np.linspace(0, 0.03, 11)
+
+    buf = 0.2  # buffer for initial transients
+    dt = 1e-3
+    tfinal = 3 + buf
+
+    def input_function(t):
+        return np.interp(t, [1, 3], [-1, 1], left=-1, right=1)
+
+    # model = nengo.Model('test_solvers', seed=290)
+    model = nengo.Model('test_regularization')
+    u = nengo.Node(output=input_function)
+    up = nengo.Probe(u)
+
+    probes = np.zeros(
+        (len(solvers), len(neurons), len(regs), len(filters)), dtype='object')
+
+    for j, n_neurons in enumerate(neurons):
+        a = nengo.Ensemble(nl_nodirect(n_neurons), dimensions=1)
+        nengo.Connection(u, a)
+
+        for i, solver in enumerate(solvers):
+            for k, reg in enumerate(regs):
+                reg_solver = lambda a, t, rng, reg=reg: solver(
+                    a, t, rng=rng, noise_amp=reg)
+                for l, filter in enumerate(filters):
+                    probes[i, j, k, l] = nengo.Probe(
+                        a, decoder_solver=reg_solver, filter=filter)
+
+    sim = nengo.Simulator(model, dt=dt)
+    sim.run(tfinal)
+    t = sim.trange()
+
+    ref = sim.data(up)
+    rmse_buf = lambda a, b: rms(a[t > buf] - b[t > buf])
+    rmses = np.zeros(probes.shape)
+    for i, probe in enumerate(probes.flat):
+        rmses.flat[i] = rmse_buf(sim.data(probe), ref)
+    rmses = rmses - rmses[:, :, [0], :]
+
+    with Plotter(Simulator, nl_nodirect) as plt:
+        plt.figure(figsize=(8, 12))
+        X, Y = np.meshgrid(filters, regs)
+
+        for i, solver in enumerate(solvers):
+            for j, n_neurons in enumerate(neurons):
+                plt.subplot(len(neurons), len(solvers), len(solvers)*j + i + 1)
+                Z = rmses[i, j, :, :]
+                plt.contourf(X, Y, Z, levels=np.linspace(Z.min(), Z.max(), 21))
+                plt.xlabel('filter')
+                plt.ylabel('reg')
+                plt.title("%s (N=%d)" % (solver.__name__, n_neurons))
+
+        plt.tight_layout()
+        plt.savefig('test_decoders.test_regularization.pdf')
+        plt.close()
+
+
+if __name__ == "__main__":
+    nengo.log(debug=True)
+    pytest.main([__file__, '-v'])
