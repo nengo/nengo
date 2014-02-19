d9637641031136843e5c08c58170a7a01bd232f0
Terry Stewart <tcstewar@uwaterloo.ca>
2014-02-19 11:07:42 -0500

    Fixed removal of passthrough nodes

--- nengo/build_utils.py
+++ nengo/build_utils.py
@@ -1,25 +1,52 @@
 from . import objects
+from . import model
 import numpy as np
 
+def generate_dot(objs, connections):
+    text = []
+    text.append('digraph G {')
+    for obj in objs:
+        text.append('  "%d" [label="%s"];'%(id(obj), obj.label))
+        
+    def label(transform):
+        if len(transform.shape)==0: return ''
+        return '%dx%d'%transform.shape
+    for c in connections:
+        text.append('  "%d" -> "%d" [label="%s"];'%(id(c.pre), id(c.post), label(c.transform)))
+    text.append('}')
+    return '\n'.join(text)
+
+
 def remove_passthrough_nodes(objs, connections):
+    
+    with open('pre.dot','w') as f:
+        f.write(generate_dot(objs, connections))
+
+
     inputs = {obj:[] for obj in objs}
     outputs = {obj:[] for obj in objs}
     
     c_removed = set()
     c_new = set()
     
+    obj_removed = set()
+    
     for c in connections:
         inputs[c.post].append(c)
         outputs[c.pre].append(c)
+        
 
     for obj in objs:
         if isinstance(obj, objects.Node) and obj.output is None:
-            print 'found passthrough node', obj
+            print 'found passthrough node', obj, id(obj)
+            obj_removed.add(obj)
             
             for c in inputs[obj]:
                 c_removed.add(c)
+                outputs[c.pre].remove(c)
             for c in outputs[obj]:
                 c_removed.add(c)
+                inputs[c.post].remove(c)
                 
             for c_in in inputs[obj]:
                 for c_out in outputs[obj]:
@@ -29,28 +56,39 @@ def remove_passthrough_nodes(objs, connections):
                     else:
                         assert c_out.filter is None
                     
-                    transform = np.dot(c_in.transform, c_out.transform)
-                    #function = 
+                    transform = np.dot(c_out.transform, c_in.transform)
+                    function = c_in.function
+                    assert c_out.function is None
                     
-                    print 'tr', c_in.transform, c_out.transform    
+                    assert c_in.pre != obj
+                    assert c_out.post != obj
                     
-                
-            
-            
-            
-            
-            
-            print '  inputs' 
-            for c in inputs[obj]:
-                print '    ',`c`
-            print '  outputs' 
-            for c in outputs[obj]:
-                print '    ',`c`
-            
-    
-            
+                    if not np.all(transform==0):
+                        dummy = model.Model()
+                        with dummy:
+                            args = {}
+                            if function is not None: args['function']=function
+                            c = objects.Connection(c_in.pre, c_out.post, 
+                                    filter=filter, transform=transform, **args)
+                        c_new.add(c)
+                        outputs[c.pre].append(c)
+                        inputs[c.post].append(c)
+                    
 
+    for c in c_new:
+        if c not in c_removed:
+            connections.append(c)
+    for c in c_removed:
+        if c not in c_new:
+            connections.remove(c)
+    for obj in obj_removed:
+        objs.remove(obj)
 
+    with open('post.dot','w') as f:
+        f.write(generate_dot(objs, connections))
+    import os
+    os.system('dot.bat -Tpng -opre.png pre.dot')
+    os.system('dot.bat -Tpng -opost.png post.dot')
             
         
             
--- nengo/tests/test_build_utils.py
+++ nengo/tests/test_build_utils.py
@@ -5,7 +5,7 @@ import nengo
 import nengo.build_utils
 
 
-def test_remove_passthrough():
+def _test_remove_passthrough():
     """Test scanning through a model and removing Nodes with output=None"""
     
     model = nengo.Model('test_remove_passthrough')
@@ -20,12 +20,34 @@ def test_remove_passthrough():
         
         nengo.Connection(input, a.input, filter=0.01)
         nengo.Connection(a.output, b.input, filter=0.01)
+        nengo.Connection(b.output, b.input, filter=0.01, transform=0.9)
+        nengo.Connection(a.output, a.input, filter=0.01, transform=np.ones((D,D)))
         nengo.Connection(b.output, output, filter=0.01)
+        
+    objs, conns = nengo.build_utils.remove_passthrough_nodes(model.objs, model.connections)
+        
+
+def test_remove_passthrough_bg():
+    """Test scanning through a model and removing Nodes with output=None"""
+    
+    model = nengo.Model('test_remove_passthrough')
+    with model:
+        D = 10
+        input = nengo.Node([1]*D, label='input')
+        def printout(t, x):
+            print t, x
+        output = nengo.Node(printout, size_in=D, label='output')
+        bg = nengo.networks.BasalGanglia(D, 20, label='BG')
+        nengo.Connection(input, bg.input, filter=0.01)
+        nengo.Connection(bg.output, output, filter=0.01)
+        
         
     objs, conns = nengo.build_utils.remove_passthrough_nodes(model.objs, model.connections)
         
     sim = nengo.Simulator(model)    
     sim.run(0.01)    
+
+
         
 
 if __name__ == "__main__":
