cdc640ad260f05a5afcbe2ef681de1792242293d
Terry Stewart <tcstewar@uwaterloo.ca>
2014-02-27 18:47:38 -0500

    Correct updating of changing model

--- main.py
+++ main.py
@@ -56,15 +56,18 @@ class NengoGui(swi.SimpleWebInterface):
             except:
                 traceback.print_exc()
                 
-        
-        nodes = []
-        node_map = {}
-        links = []
-        for obj in model.objs:
-            node_map[obj] = len(nodes)
-            nodes.append(dict(label=obj.label, line=3))
-        for c in model.connections:
-            links.append(dict(source=node_map[c.pre], target=node_map[c.post]))
+        try:
+            nodes = []
+            node_map = {}
+            links = []
+            for obj in model.objs:
+                node_map[obj] = len(nodes)
+                nodes.append(dict(label=obj.label, line=3, id=len(nodes)))
+            for c in model.connections:
+                links.append(dict(source=node_map[c.pre], target=node_map[c.post], id=len(links)))
+        except:
+            traceback.print_exc()
+            return json.dumps(dict(error_line=2, text='Unknown'))
             
         
         '''
