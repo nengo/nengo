f7cc8f1b42fbc3e34b4faf6136d0dc6349b1cb6d
Trevor Bekolay <tbekolay@gmail.com>
2014-03-28 09:57:36 -0400

    Moved execfile to nengo.utils.compat
    - Also made the implementation a bit safer

--- nengo/utils/compat.py
+++ nengo/utils/compat.py
@@ -29,3 +29,23 @@ def is_number(obj, check_complex=False):
 def is_string(obj):
     types = str if IS_PYTHON3 else basestring
     return isinstance(obj, types)
+
+
+def execfile(path, globals, locals=None):
+    """Execute a Python script in the (mandatory) globals namespace.
+
+    This is similar to the Python 2 builtin execfile, but it
+    also works on Python 3, and ``globals`` is mandatory.
+    This is because getting the calling frame's globals would
+    be non-trivial, and it makes sense to be explicit about
+    the namespace being modified.
+
+    If ``locals`` is not specified, it will have the same value
+    as ``globals``, as in the execfile builtin.
+    """
+    if locals is None:
+        locals = globals
+    with open(path, 'rb') as fp:
+        source = fp.read()
+    code = compile(source, path, "exec")
+    exec(code, globals, locals)
