81db6a8b66c1ae18a8a4a28cc8e9623147dd5104
Trevor Bekolay <tbekolay@gmail.com>
2014-03-31 10:37:27 -0400

    Added utils.graph which simulator uses
    All we ever used networkx for was topographical sort.
    This commit adds an implementation of topographical sort
    to utils.graphs, which nengo.Simulator uses instead of networkx.
    
    Other changes:
    - Moved _init_dg to utils.simulator as operator_dependency_graph
    - Refactored operator_dependency_graph slightly
    - Implemented unit tests for utils.graphs

--- nengo/simulator.py
+++ nengo/simulator.py
@@ -6,14 +6,14 @@ Reference simulator for nengo models.
 
 from __future__ import print_function
 
-from collections import defaultdict, Mapping
-import itertools
+from collections import Mapping
 import logging
 
-import networkx as nx
 import numpy as np
 
 from nengo.builder import Builder
+from nengo.utils.graphs import toposort
+from nengo.utils.simulator import operator_depencency_graph
 
 logger = logging.getLogger(__name__)
 
@@ -102,9 +102,8 @@ class Simulator(object):
         for op in self.model.operators:
             op.init_sigdict(self._sigdict, self.dt)
 
-        self.dg = self._init_dg()
-        self._step_order = [node
-                            for node in nx.topological_sort(self.dg)
+        self.dg = operator_depencency_graph(self.model.operators)
+        self._step_order = [node for node in toposort(self.dg)
                             if hasattr(node, 'make_step')]
         self._steps = [node.make_step(self._sigdict, self.dt)
                        for node in self._step_order]
@@ -116,95 +115,6 @@ class Simulator(object):
 
         # Provide a nicer interface to probe outputs
         self.data = ProbeDict(self._probe_outputs)
-
-    def _init_dg(self, verbose=False):  # noqa
-        operators = self.model.operators
-        dg = nx.DiGraph()
-
-        for op in operators:
-            dg.add_edges_from(itertools.product(op.reads + op.updates, [op]))
-            dg.add_edges_from(itertools.product([op], op.sets + op.incs))
-
-        # -- all views of a base object in a particular dictionary
-        by_base_writes = defaultdict(list)
-        by_base_reads = defaultdict(list)
-        reads = defaultdict(list)
-        sets = defaultdict(list)
-        incs = defaultdict(list)
-        ups = defaultdict(list)
-
-        for op in operators:
-            for node in op.sets + op.incs:
-                by_base_writes[node.base].append(node)
-
-            for node in op.reads:
-                by_base_reads[node.base].append(node)
-
-            for node in op.reads:
-                reads[node].append(op)
-
-            for node in op.sets:
-                sets[node].append(op)
-
-            for node in op.incs:
-                incs[node].append(op)
-
-            for node in op.updates:
-                ups[node].append(op)
-
-        # -- assert that only one op sets any particular view
-        for node in sets:
-            assert len(sets[node]) == 1, (node, sets[node])
-
-        # -- assert that only one op updates any particular view
-        for node in ups:
-            assert len(ups[node]) == 1, (node, ups[node])
-
-        # --- assert that any node that is incremented is also set/updated
-        for node in incs:
-            assert len(sets[node] + ups[node]) > 0, (node)
-
-        # -- assert that no two views are both set and aliased
-        if len(sets) >= 2:
-            for node, other in itertools.combinations(sets, 2):
-                assert not node.shares_memory_with(other), \
-                    ("%s shares memory with %s" % (node, other))
-
-        # -- assert that no two views are both updated and aliased
-        if len(ups) >= 2:
-            for node, other in itertools.combinations(ups, 2):
-                assert not node.shares_memory_with(other), (node, other)
-
-        # -- Scheduling algorithm for serial evaluation:
-        #    1) All sets on a given base signal
-        #    2) All incs on a given base signal
-        #    3) All reads on a given base signal
-        #    4) All updates on a given base signal
-
-        # -- incs depend on sets
-        for node, post_ops in incs.items():
-            pre_ops = list(sets[node])
-            for other in by_base_writes[node.base]:
-                pre_ops += sets[other]
-            dg.add_edges_from(itertools.product(set(pre_ops), post_ops))
-
-        # -- reads depend on writes (sets and incs)
-        for node, post_ops in reads.items():
-            pre_ops = sets[node] + incs[node]
-            for other in by_base_writes[node.base]:
-                pre_ops += sets[other] + incs[other]
-            dg.add_edges_from(itertools.product(set(pre_ops), post_ops))
-
-        # -- updates depend on reads, sets, and incs.
-        for node, post_ops in ups.items():
-            pre_ops = sets[node] + incs[node] + reads[node]
-            for other in by_base_writes[node.base]:
-                pre_ops += sets[other] + incs[other] + reads[other]
-            for other in by_base_reads[node.base]:
-                pre_ops += sets[other] + incs[other] + reads[other]
-            dg.add_edges_from(itertools.product(set(pre_ops), post_ops))
-
-        return dg
 
     @property
     def signals(self):
