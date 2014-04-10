3a186ecc0a0325e53fd74c8b07d8ea9e4812524d
Aaron Voelker <arvoelke@gmail.com>
2014-04-10 15:54:06 -0400

    Product network for multiplying vectors

--- nengo/networks/__init__.py
+++ nengo/networks/__init__.py
@@ -3,3 +3,4 @@ from .circularconvolution import CircularConvolution
 from .integrator import Integrator
 from .oscillator import Oscillator
 from .ensemblearray import EnsembleArray
+from .product import Product
