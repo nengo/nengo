65d8e3bb1f71cd80b9cefd8c224c73d37404f4b4
Eric Hunsberger <erichuns@gmail.com>
2014-04-14 22:20:17 -0400

    Set seed in `test_integrator`.

--- nengo/networks/tests/test_integrator.py
+++ nengo/networks/tests/test_integrator.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 def test_integrator(Simulator, nl):
-    model = nengo.Network(label='Integrator')
+    model = nengo.Network(label='Integrator', seed=892)
     with model:
         inputs = {0: 0, 0.2: 1, 1: 0, 2: -2, 3: 0, 4: 1, 5: 0}
         input = nengo.Node(piecewise(inputs))
