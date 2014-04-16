7b73dee22f9e5e8d0e903fc492af913b2e59bf88
Trevor Bekolay <tbekolay@gmail.com>
2014-04-16 14:50:15 -0400

    Simplified CircularConvolution
    We're no longer bypassing product.A and product.B.
    This involved some rearranging of CircularConvolution
    to attempt to make it more understandable.
    
    Shoutout to @hunse for fixing the circularconv transforms test
    and explaining how the network works!

--- nengo/networks/circularconvolution.py
+++ nengo/networks/circularconvolution.py
@@ -19,85 +19,101 @@ def circconv(a, b, invert_a=False, invert_b=False, axis=-1):
 class CircularConvolution(nengo.Network):
     """CircularConvolution docs XXX"""
 
-    def __init__(self, neurons, dimensions, radius=1,
-                 invert_a=False, invert_b=False):
-        self.transformA = self._input_transform(
-            dimensions, first=True, invert=invert_a)
-        self.transformB = self._input_transform(
-            dimensions, first=False, invert=invert_b)
-        self.transformC = self._output_transform(dimensions)
+    def __init__(self, neurons, dimensions, invert_a=False, invert_b=False):
+        self.dimensions = dimensions
+        self.invert_a = invert_a
+        self.invert_b = invert_b
 
         self.A = nengo.Node(size_in=dimensions, label="A")
         self.B = nengo.Node(size_in=dimensions, label="B")
-        self.product = Product(neurons, self.transformC.shape[1], label="conv")
+        self.product = Product(
+            neurons, self.transform_out.shape[1], label="conv")
         self.output = nengo.Node(size_in=dimensions, label="output")
 
-        # Connect into product.product.input rather than product.A and
-        # product.B in order to avoid writing the transforms in terms ot the
-        # partioned dimensions.
+        nengo.Connection(self.A, self.product.A,
+                         transform=self.transformA, synapse=None)
+        nengo.Connection(self.B, self.product.B,
+                         transform=self.transformB, synapse=None)
         nengo.Connection(
-            self.A, self.product.product.input, transform=self.transformA,
-            synapse=None)
-        nengo.Connection(
-            self.B, self.product.product.input, transform=self.transformB,
-            synapse=None)
-        nengo.Connection(
-            self.product.output, self.output, transform=self.transformC,
+            self.product.output, self.output, transform=self.transform_out,
             synapse=None)
 
-    @classmethod
-    def _input_transform(cls, dims, first, invert=False):
-        dims2 = 4 * (dims // 2 + 1)
-        T = np.zeros((dims2, 2, dims))
-        dft = cls.dft_half(dims)
+    @property
+    def transformA(self):
+        return self._input_transform(self.dimensions, 'A', self.invert_a)
 
-        for i in range(dims2):
-            row = dft[i // 4] if not invert else dft[i // 4].conj()
-            if first:
-                T[i, 0] = row.real if i % 2 == 0 else row.imag
-            else:
-                T[i, 1] = row.real if i % 4 == 0 or i % 4 == 3 else row.imag
+    @property
+    def transformB(self):
+        return self._input_transform(self.dimensions, 'B', self.invert_b)
 
-        # --- Throw away rows that we don't need (b/c they're zero)
-        i = np.arange(dims2)
-        if dims % 2 == 0:
-            T = T[(i == 0) | (i > 3) & (i < len(i) - 3)]
-        else:
-            T = T[(i == 0) | (i > 3)]
-
-        return T.reshape((-1, dims))
-
-    @classmethod
-    def _output_transform(cls, dims):
+    @property
+    def transform_out(self):
+        dims = self.dimensions
         dims2 = (dims // 2 + 1)
-        T = np.zeros((dims2, 4, dims))
-        idft = cls.dft_half(dims).conj()
+        tr = np.zeros((dims2, 4, dims))
+        idft = self.dft_half(dims).conj()
 
         for i in range(dims2):
             row = idft[i] if i == 0 or 2*i == dims else 2*idft[i]
-            T[i, 0] = row.real
-            T[i, 1] = -row.real
-            T[i, 2] = -row.imag
-            T[i, 3] = -row.imag
+            tr[i, 0] = row.real
+            tr[i, 1] = -row.real
+            tr[i, 2] = -row.imag
+            tr[i, 3] = -row.imag
 
-        T = T.reshape(4*dims2, dims)
-
-        # --- Throw away rows that we don't need (b/c they're zero)
-        i = np.arange(4*dims2)
-        if dims % 2 == 0:
-            T = T[(i == 0) | (i > 3) & (i < len(i) - 3)]
-        else:
-            T = T[(i == 0) | (i > 3)]
-
+        tr = tr.reshape(4*dims2, dims)
+        self._remove_imag_rows(tr)
         # scaling is needed since we have 1./sqrt(dims) in DFT
-        T *= np.sqrt(dims)
+        tr *= np.sqrt(dims)
 
-        return T.T
+        return tr.T
 
-    @classmethod
+    @staticmethod
     @memoize
-    def dft_half(cls, n):
+    def dft_half(n):
         x = np.arange(n)
         w = np.arange(n // 2 + 1)
         return ((1. / np.sqrt(n)) *
                 np.exp((-2.j * np.pi / n) * (w[:, None] * x[None, :])))
+
+    @staticmethod
+    @memoize
+    def _input_transform(dims, align, invert):
+        """Create a transform to map the input into the Fourier domain.
+
+        See the class docstring for more details.
+
+        Parameters
+        ----------
+        dims : int
+            Input dimensions.
+        align : 'A' or 'B'
+            How to align the real and imaginary components; the alignment
+            depends on whether we're doing transformA or transformB.
+        invert : bool
+            Whether to reverse the order of elements.
+        """
+        if align not in ('A', 'B'):
+            raise ValueError("'align' must be either 'A' or 'B'")
+
+        dims2 = 4 * (dims // 2 + 1)
+        tr = np.zeros((dims2, dims))
+        dft = CircularConvolution.dft_half(dims)
+
+        for i in range(dims2):
+            row = dft[i // 4] if not invert else dft[i // 4].conj()
+            if align == 'A':
+                tr[i] = row.real if i % 2 == 0 else row.imag
+            else:  # align == 'B'
+                tr[i] = row.real if i % 4 == 0 or i % 4 == 3 else row.imag
+
+        CircularConvolution._remove_imag_rows(tr)
+        return tr.reshape((-1, dims))
+
+    @staticmethod
+    def _remove_imag_rows(tr):
+        """Throw away imaginary row we don't need (since they're zero)"""
+        i = np.arange(tr.shape[0])
+        if tr.shape[1] % 2 == 0:
+            tr = tr[(i == 0) | (i > 3) & (i < len(i) - 3)]
+        else:
+            tr = tr[(i == 0) | (i > 3)]
