e76ff0b4a6be6ce7d6e9490baa6690741f73d2d4
Jan Gosmann <jan@hyper-world.de>
2014-05-02 17:35:32 -0400

    Test caching with E argument to solver.

--- nengo/tests/test_cache.py
+++ nengo/tests/test_cache.py
@@ -34,6 +34,7 @@ def test_decoder_cache(tmpdir):
     targets = np.ones((M, N))
     rng = np.random.RandomState(42)
 
+    # Basic test, that results are cached.
     cache = DecoderCache(cache_dir)
     decoders1, solver_info1 = cache.wrap_solver(solver_mock.get_solver_fn())(
         activities, targets, rng)
@@ -44,11 +45,38 @@ def test_decoder_cache(tmpdir):
     assert_equal(decoders1, decoders2)
     assert solver_info1 == solver_info2
 
+    decoders3, solver_info3 = cache.wrap_solver(solver_mock.get_solver_fn())(
+        2 * activities, targets, rng)
+    assert solver_mock.n_calls == 2
+    assert np.any(decoders1 != decoders3)
+
+    # Test that the cache does not load results of another solver.
     another_solver = DecoderSolverMock('another_solver')
     cache.wrap_solver(another_solver.get_solver_fn())(activities, targets, rng)
     assert another_solver.n_calls == 1
 
-    # TODO test using E
+
+def test_decoder_cache_with_E_argument_to_solver(tmpdir):
+    cache_dir = str(tmpdir)
+    solver_mock = DecoderSolverMock()
+
+    M = 100
+    N = 10
+    N2 = 5
+    D = 2
+    activities = np.ones((M, D))
+    targets = np.ones((M, N))
+    rng = np.random.RandomState(42)
+    E = np.ones((D, N2))
+
+    cache = DecoderCache(cache_dir)
+    decoders1, solver_info1 = cache.wrap_solver(solver_mock.get_solver_fn())(
+        activities, targets, rng, E=E)
+    assert solver_mock.n_calls == 1
+    decoders2, solver_info2 = cache.wrap_solver(solver_mock.get_solver_fn())(
+        activities, targets, rng, E=E)
+    assert solver_mock.n_calls == 1  # check the result is read from cache
+    assert_equal(decoders1, decoders2)
 
 
 if __name__ == "__main__":
