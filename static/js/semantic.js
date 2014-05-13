3bc296ddc9497d23ea170174043b195df2f6a292
Terry Stewart <tcstewar@uwaterloo.ca>
2014-05-13 18:11:29 -0400

    Fixed display of recurrent connection in semantic zoom mode

--- static/js/semantic.js
+++ static/js/semantic.js
@@ -4,6 +4,9 @@ function fix_node_scales(scale) {
     
     container.selectAll('polyline').style('stroke-width', ''+(2.0/scale)+'px');
     container.selectAll('g.node_net rect').attr('rx', 15.0/scale).attr('ry', 15.0/scale);
+    
+    d3.select('#recur')
+        .attr('transform', 'scale(' + (1.0 / scale)+')translate(-20,-34)')
 }
 
 function fix_labels(scale) {
