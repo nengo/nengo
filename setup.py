19d80afe956eecbf66ce478e28ead7e935ead4a0
Trevor Bekolay <tbekolay@gmail.com>
2014-05-19 10:39:45 -0400

    Force setuptools
    If we can be assured that we're using setuptools,
    we can make setup.py a lot more effective. This should be possible,
    as the ez_setup.py script (now included) should be able to download
    and install setuptools on all OSes.
    
    A few nice things if we can rely on setuptools
    - simpler setup.py
    - use find_packages function instead of having to edit setup.py
      when we add a new package

--- setup.py
+++ setup.py
@@ -1,36 +1,27 @@
 #!/usr/bin/env python
+import imp
 import sys
 import os
-import imp
 
 try:
     from setuptools import setup
-    from setuptools.command.test import test as TestCommand
 except ImportError:
-    try:
-        from ez_setup import use_setuptools
-        use_setuptools()
-        from setuptools import setup
-        from setuptools.command.test import test as TestCommand
-    except Exception as e:
-        print("Forget setuptools, trying distutils...")
-        from distutils.core import setup
+    from ez_setup import use_setuptools
+    use_setuptools()
 
-try:
-    class Tox(TestCommand):
-        def finalize_options(self):
-            TestCommand.finalize_options(self)
-            self.test_args = []
-            self.test_suite = True
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
 
-        def run_tests(self):
-            #import here, cause outside the eggs aren't loaded
-            import tox
-            errno = tox.cmdline(self.test_args)
-            sys.exit(errno)
-    testing = {'tests_require': ['tox'], 'cmdclass': {'test': Tox}}
-except NameError:
-    testing = {}
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
 
 root = os.path.dirname(os.path.realpath(__file__))
 version_module = imp.load_source(
@@ -39,12 +30,13 @@ description = ("Tools for making neural simulations using the methods "
                + "of the Neural Engineering Framework")
 with open(os.path.join(root, 'README.rst')) as readme:
     long_description = readme.read()
+
 setup(
     name="nengo",
     version=version_module.version,
     author="CNRGlab at UWaterloo",
     author_email="celiasmith@uwaterloo.ca",
-    packages=['nengo', 'nengo.tests', 'nengo.networks'],
+    packages=find_packages(),
     scripts=[],
     url="https://github.com/ctn-waterloo/nengo",
     license="GPLv3",
@@ -54,5 +46,6 @@ setup(
         "numpy (>=1.5.0)",
         "networkx",
     ],
-    **testing
+    tests_require=['tox'],
+    cmdclass={'test': Tox}
 )
