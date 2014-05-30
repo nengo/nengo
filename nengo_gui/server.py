42c8e2c66156def16bd3fc787f801268195c47b9
Terry Stewart <tcstewar@uwaterloo.ca>
2014-05-30 17:49:28 -0400

    Moved server code into nengo_gui/server.py (#56)

--- nengo_gui/server.py
+++ nengo_gui/server.py
@@ -176,29 +176,3 @@ class NengoGui(nengo_gui.swi.SimpleWebInterface):
 
         conv = nengo_gui.converter.Converter(model, code.splitlines(), locals, cfg)
         return conv.to_json()
-
-
-if __name__=='__main__':
-    import optparse
-    parser = optparse.OptionParser()
-    parser.add_option('-p', '--password', dest='password', metavar='PASS',
-                      default=None, help='password for remote access')
-    parser.add_option('-r', '--refresh', dest='refresh', metavar='TIME',
-                      default=0, help='interval to check server for changes',
-                      type='int')
-    parser.add_option('-P', '--port', dest='port', metavar='PORT',
-                      default=8080, type='int', help='port to run server on')
-    (options, args) = parser.parse_args()
-
-    NengoGui.set_refresh_interval(options.refresh)
-
-    if len(args) > 0:
-        NengoGui.set_default_filename(args[0])
-    addr = 'localhost'
-    if options.password is not None:
-        nengo_gui.swi.addUser('', options.password)
-        addr = ''   # allow connections from anywhere
-    else:
-        nengo_gui.swi.browser(options.port)
-
-    nengo_gui.swi.start(NengoGui, options.port, addr=addr)
