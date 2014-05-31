d2fc70138632a87b05e9d91dfa2ee3481124a0aa
Terry Stewart <tcstewar@uwaterloo.ca>
2014-05-31 16:37:48 -0400

    Updated default.py to have examples of input and probing

--- nengo_gui/scripts/default.py
+++ nengo_gui/scripts/default.py
@@ -2,9 +2,12 @@ import nengo
 
 model = nengo.Network(label="My Network")
 with model:
-    a = nengo.Ensemble(n_neurons=10, dimensions=2, label="My Ensemble")
+    input = nengo.Node([0, 0])
 
-    nengo.Connection(a, a)
+    a = nengo.Ensemble(100, dimensions=2, label="My Ensemble")
+    nengo.Probe(a)
+
+    nengo.Connection(input, a)
 
 import nengo_gui
 gui = nengo_gui.Config()
@@ -12,3 +15,5 @@ gui[model].scale = 1.0
 gui[model].offset = 0.0,0.0
 gui[a].pos = 300.0, 200.0
 gui[a].scale = 1.000
+gui[input].pos = 100.0, 200.0
+gui[input].scale = 1.000
