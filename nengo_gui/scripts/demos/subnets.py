e4ea029ed8bf5124c127dee779a94463c1f1ed50
Chris Eliasmith <celiasmi@subcortex.uwaterloo.ca>
2014-06-05 15:16:36 -0400

    fixed subnets.py

--- nengo_gui/scripts/demos/subnets.py
+++ nengo_gui/scripts/demos/subnets.py
@@ -17,13 +17,3 @@ with model:
 		d = nengo.Ensemble(n_neurons=80, dimensions=2)
 		e = nengo.Ensemble(n_neurons=80, dimensions=2)
 
-
-import nengo_gui
-gui = nengo_gui.Config()
-gui[model].scale = 0.9986146661010299
-gui[model].offset = 0.5457427618872543,0.24795939341674966
-gui[a].pos = 200.000, 94.000
-gui[b].pos = 113.875, 227.280
-gui[c].pos = 300.614, 227.072
-gui[d].pos = 313.046, 344.705
-gui[e].pos = 115.772, 344.706
