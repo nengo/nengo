06564500278307b3d8a520d08cb1d421dd62ed9b
Youssef Zaky <youssefzaky@gmail.com>
2014-06-27 13:49:07 -0400

    Added target_function helper function
    This is for making connections when you have the eval_points
    and targets, but don't necessarily know the underlying function.

---
+++ nengo/utils/connection.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import
+
+import numpy as np
+
+import nengo.utils.numpy as npext
+
+
+def target_function(eval_points, targets):
+    """Get a function that maps evaluation points to target points.
+
+    Use this when making a nengo connection using a sequence
+    of evaluation points and targets, instead of passing a
+    callable as the connection function.
+
+    Parameters
+    ----------
+    eval_points: iterable
+        A sequence of evaluation points.
+    targets: iterable
+        A sequence of targets with the same length as ``eval_points``
+
+    Returns
+    -------
+    dict:
+       A diciontary with two keys: ``function`` and ``eval_points``.
+       function is the mapping between the evaluation points and the
+       targets. ``eval_points`` are the evalutaion points that will
+       be passed to the connection
+
+    Examples
+    --------
+    ens1 = nengo.Ensemble(n_neurons=100, dimensions=1)
+    ens2 = nengo.Ensemble(n_neurons=100, dimensions=1)
+    eval_points = numpy.arange(-1, 1, 0.01)
+    targets = numpy.sin(eval_points)
+    #the transformation on this connection approximates a sin function
+    nengo.Connection(ens1, ens2,
+                     **target_function(eval_points, targets)
+    """
+
+    eval_points = npext.array(eval_points, dtype=np.float64, min_dims=2)
+    targets = npext.array(targets, dtype=np.float64, min_dims=2)
+
+    if len(eval_points) != len(targets):
+        raise ValueError("Number of evaluation points %s "
+                         "is not equal to number of targets "
+                         "%s" % (len(eval_points), len(targets)))
+
+    func_dict = {}
+    for eval_point, target in zip(eval_points, targets):
+        func_dict[tuple(eval_point)] = target
+
+    def function(x):
+        x = tuple(x)
+        return func_dict[x]
+
+    return {'function': function, 'eval_points': eval_points}
