a0f9ddaad49bcb296ad02a1abfde65654eebd7b4
Terry Stewart <tcstewar@uwaterloo.ca>
2014-07-23 13:21:45 -0400

    Updated to most recent nengo-temp

--- nengo/vis/config.py
+++ nengo/vis/config.py
@@ -1,17 +1,18 @@
 import nengo
 import nengo.config
+import nengo.params
 
 class Config(nengo.config.Config):
     def __init__(self):
         super(Config, self).__init__()
         for klass in [nengo.Ensemble, nengo.Node]:
             self.configures(klass)
-            self[klass].set_param('pos', nengo.config.Parameter(None))
-            self[klass].set_param('scale', nengo.config.Parameter(None))
-            self[klass].set_param('size', nengo.config.Parameter((50,50)))
+            self[klass].set_param('pos', nengo.params.Parameter(None))
+            self[klass].set_param('scale', nengo.params.Parameter(None))
+            self[klass].set_param('size', nengo.params.Parameter((50,50)))
 
         self.configures(nengo.Network)
-        self[nengo.Network].set_param('scale', nengo.config.Parameter(None))
-        self[nengo.Network].set_param('offset', nengo.config.Parameter((0,0)))
-        self[nengo.Network].set_param('pos', nengo.config.Parameter(None))
-        self[nengo.Network].set_param('size', nengo.config.Parameter(None)) #width/height
+        self[nengo.Network].set_param('scale', nengo.params.Parameter(None))
+        self[nengo.Network].set_param('offset', nengo.params.Parameter((0,0)))
+        self[nengo.Network].set_param('pos', nengo.params.Parameter(None))
+        self[nengo.Network].set_param('size', nengo.params.Parameter(None)) #width/height
