e94569f4c378d7c7f32f22ef3510777e83efa3f6
Eric Hunsberger <erichuns@gmail.com>
2014-08-19 11:15:00 -0400

    Improvements to pre-slicing.
    - Slicing is now done on signalviews or with a PyFunc, rather
      than via the transform. This is more efficient.
    - Also fixed a bug with pre-slicing. Fixes #467.

--- nengo/networks/tests/test_product.py
+++ nengo/networks/tests/test_product.py
@@ -15,32 +15,30 @@ def test_sine_waves(Simulator, nl):
 
     func_A = lambda t: np.sqrt(radius)*np.sin(np.arange(1, dim+1)*2*np.pi*t)
     func_B = lambda t: np.sqrt(radius)*np.sin(np.arange(dim, 0, -1)*2*np.pi*t)
-    pstc = 0.003
     with product:
         input_A = nengo.Node(func_A)
         input_B = nengo.Node(func_B)
         nengo.Connection(input_A, product.A)
         nengo.Connection(input_B, product.B)
-        p = nengo.Probe(product.output, synapse=pstc)
+        p = nengo.Probe(product.output, synapse=0.005)
 
     sim = Simulator(product)
     sim.run(1.0)
 
     t = sim.trange()
     AB = np.asarray(list(map(func_A, t))) * np.asarray(list(map(func_B, t)))
-    delay = 0.011
-    offset = np.where(t > delay)[0]
+    delay = 0.013
+    offset = np.where(t >= delay)[0]
 
     with Plotter(Simulator, nl) as plt:
         for i in range(dim):
             plt.subplot(dim+1, 1, i+1)
-            plt.plot(t + delay, AB[:, i], label="$A \cdot B$")
-            plt.plot(t, sim.data[p][:, i], label="Output")
-            plt.legend()
+            plt.plot(t + delay, AB[:, i])
+            plt.plot(t, sim.data[p][:, i])
         plt.savefig('test_product.test_sine_waves.pdf')
         plt.close()
 
-    assert rmse(AB[:len(offset), :], sim.data[p][offset, :]) < 0.3
+    assert rmse(AB[:len(offset), :], sim.data[p][offset, :]) < 0.2
 
 
 if __name__ == "__main__":
