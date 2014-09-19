b352315e629990f3b9a0f91d168c14dd3ef67996
Terry Stewart <tcstewar@uwaterloo.ca>
2014-09-19 18:01:40 -0400

    Fix circular convolution radius

--- nengo/networks/tests/test_circularconv.py
+++ nengo/networks/tests/test_circularconv.py
@@ -4,11 +4,8 @@ import numpy as np
 import pytest
 
 import nengo
-from nengo.networks import EnsembleArray
 from nengo.networks.circularconvolution import circconv
-from nengo.utils.compat import range
 from nengo.utils.numpy import rmse
-from nengo.utils.testing import Plotter
 
 logger = logging.getLogger(__name__)
 
@@ -32,79 +29,29 @@ def test_circularconv_transforms(invert_a, invert_b):
     assert np.allclose(z0, z1)
 
 
-def test_circularconv(Simulator, nl, dims=4, neurons_per_product=128):
+@pytest.mark.parametrize('dims', [4, 32])
+def test_neural_accuracy(Simulator, dims, neurons_per_product=128):
     rng = np.random.RandomState(4238)
-
-    n_neurons = neurons_per_product
-    n_neurons_d = 2 * neurons_per_product
-    radius = 1
-
     a = rng.normal(scale=np.sqrt(1./dims), size=dims)
     b = rng.normal(scale=np.sqrt(1./dims), size=dims)
     result = circconv(a, b)
-    assert np.abs(a).max() < radius
-    assert np.abs(b).max() < radius
-    assert np.abs(result).max() < radius
 
-    # --- model
-    model = nengo.Network(label="circular convolution")
+    model = nengo.Network(label="circular conv", seed=1)
+    model.config[nengo.Ensemble].neuron_type = nengo.LIFRate()
     with model:
-        model.config[nengo.Ensemble].neuron_type = nl()
         inputA = nengo.Node(a)
         inputB = nengo.Node(b)
-        A = EnsembleArray(n_neurons, dims, radius=radius)
-        B = EnsembleArray(n_neurons, dims, radius=radius)
         cconv = nengo.networks.CircularConvolution(
-            n_neurons_d, dimensions=dims)
-        res = EnsembleArray(n_neurons, dims, radius=radius)
-
-        nengo.Connection(inputA, A.input)
-        nengo.Connection(inputB, B.input)
-        nengo.Connection(A.output, cconv.A)
-        nengo.Connection(B.output, cconv.B)
-        nengo.Connection(cconv.output, res.input)
-
-        A_p = nengo.Probe(A.output, synapse=0.03)
-        B_p = nengo.Probe(B.output, synapse=0.03)
-        res_p = nengo.Probe(res.output, synapse=0.03)
-
-    # --- simulation
+            neurons_per_product, dimensions=dims)
+        nengo.Connection(inputA, cconv.A, synapse=None)
+        nengo.Connection(inputB, cconv.B, synapse=None)
+        res_p = nengo.Probe(cconv.output)
     sim = Simulator(model)
-    sim.run(1.0)
+    sim.run(0.01)
 
-    t = sim.trange()
+    error = rmse(result, sim.data[res_p][-1])
 
-    with Plotter(Simulator, nl) as plt:
-        def plot(actual, probe, title=""):
-            ref_y = np.tile(actual, (len(t), 1))
-            sim_y = sim.data[probe]
-            colors = ['b', 'g', 'r', 'c', 'm', 'y']
-            for i in range(min(dims, len(colors))):
-                plt.plot(t, ref_y[:, i], '--', color=colors[i])
-                plt.plot(t, sim_y[:, i], '-', color=colors[i])
-                plt.title(title)
-
-        plt.subplot(311)
-        plot(a, A_p, title="A")
-        plt.subplot(312)
-        plot(b, B_p, title="B")
-        plt.subplot(313)
-        plot(result, res_p, title="Result")
-        plt.tight_layout()
-        plt.savefig('test_circularconv.test_circularconv_%d.pdf' % dims)
-        plt.close()
-
-    # --- results
-    tmask = t > (0.5 + sim.dt/2)
-    assert sim.data[A_p][tmask].shape == (500, dims)
-    a_sim = sim.data[A_p][tmask].mean(axis=0)
-    b_sim = sim.data[B_p][tmask].mean(axis=0)
-    res_sim = sim.data[res_p][tmask].mean(axis=0)
-
-    rtol, atol = 0.1, 0.05
-    assert np.allclose(a, a_sim, rtol=rtol, atol=atol)
-    assert np.allclose(b, b_sim, rtol=rtol, atol=atol)
-    assert rmse(result, res_sim) < 0.075
+    assert error < 0.1
 
 
 if __name__ == "__main__":
