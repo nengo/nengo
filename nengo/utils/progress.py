dfbf1678512751e9cf5e34372eab6339e858a18a
Jan Gosmann <jan@hyper-world.de>
2014-10-03 20:18:32 -0400

    Add ETA and seconds_passed to progress bar.

--- nengo/utils/progress.py
+++ nengo/utils/progress.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 import os
 import sys
+import time
 
 from nengo.utils.compat import get_terminal_size
 
@@ -11,13 +12,30 @@ class ProgressBar(object):
         self.steps = 0
         self.max_steps = max_steps
         self.last_update = 0
+        self.start_time = self.end_time = time.time()
+        self.finished = False
         self.update_interval = update_interval
 
     @property
     def progress(self):
         return self.steps / self.max_steps
 
+    @property
+    def seconds_passed(self):
+        if self.finished:
+            return self.end_time - self.start_time
+        else:
+            return time.time() - self.start_time
+
+    @property
+    def eta(self):
+        if self.progress > 0.:
+            return (1. - self.progress) * self.seconds_passed / self.progress
+        else:
+            return -1
+
     def start(self):
+        self.finished = False
         self.steps = 0
         self.last_update = 0
         self._on_start()
@@ -27,6 +45,10 @@ class ProgressBar(object):
         raise NotImplementedError()
 
     def finish(self):
+        if success:
+            self.steps = self.max_steps
+        self.end_time = time.time()
+        self.finished = True
         self._on_finish()
 
     def _on_finish(self):
