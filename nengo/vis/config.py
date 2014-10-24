eaf62842d91a312b5287f62a163c9c24ac58ab68
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-10-24 18:58:36 -0400

    Implement D3DataRenderer.
    Needs some unit tests, but seems to work.

--- nengo/vis/config.py
+++ nengo/vis/config.py
@@ -9,12 +9,12 @@ class Config(nengo.config.Config):
         super(Config, self).__init__()
         for klass in [nengo.Ensemble, nengo.Node]:
             self.configures(klass)
-            self[klass].set_param('pos', nengo.params.Parameter(None))
-            self[klass].set_param('scale', nengo.params.Parameter(None))
-            self[klass].set_param('size', nengo.params.Parameter((50,50)))
+            self[klass].set_param('pos', nengo.params.Parameter((0, 0)))
+            self[klass].set_param('scale', nengo.params.Parameter(1))
+            self[klass].set_param('size', nengo.params.Parameter((50, 50)))
 
         self.configures(nengo.Network)
         self[nengo.Network].set_param('scale', nengo.params.Parameter(1.))
         self[nengo.Network].set_param('offset', nengo.params.Parameter((0,0)))
-        self[nengo.Network].set_param('pos', nengo.params.Parameter(None))
-        self[nengo.Network].set_param('size', nengo.params.Parameter(None)) #width/height
+        self[nengo.Network].set_param('pos', nengo.params.Parameter((-50, -50)))
+        self[nengo.Network].set_param('size', nengo.params.Parameter((100, 100))) #width/height
