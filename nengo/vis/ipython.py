557fef87c52d2a788e54936629eac14759a6a515
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-10-28 18:02:26 -0400

    Make nengo.vis resources accessible.

--- nengo/vis/ipython.py
+++ nengo/vis/ipython.py
@@ -18,14 +18,61 @@ class SimpleIdentificator(Identificator):
         return id(obj)
 
 
+def get_main_js():
+    return pkgutil.get_data('nengo.vis', 'static/js/main.js')
+
+
+def get_d3_js():
+    return pkgutil.get_data('nengo.vis', 'static/js/vendor/d3.min.js')
+
+
+def get_svg_css():
+    return pkgutil.get_data('nengo.vis', 'static/css/graph.css')
+
+
+SVG_TEMPLATE = '''
+    <svg version="1.1">
+        <defs>
+            // the arrow on the links
+            <marker id="TriangleMarker"
+                    viewBox="0 0 10 10" refX="0" refY="5"
+                    markerUnits="strokeWidth"
+                    markerWidth="6" markerHeight="4"
+                    orient="auto">
+                <path d="M 0 0 L 10 5 L 0 10 z" />
+            </marker>
+            <g id ="ensemble" transform="translate(-17.2735,-17.7015)">
+                <circle id="mainCircle" cx="16" cy="18" r="18"/>
+                <circle cx="4.843" cy="10.519" r="4.843"/>
+                <circle cx="16.186" cy="17.873" r="4.843"/>
+                <circle cx="21.012" cy="30.56" r="4.843"/>
+                <circle cx="29.704" cy="17.229" r="4.843"/>
+                <circle cx="5.647" cy="26.413" r="4.843"/>
+                <circle cx="19.894" cy="4.842" r="4.843"/>
+            </g>
+            <g id = "recur">
+                <path
+                    d="M6.451,28.748C2.448,26.041,0,22.413,0,18.425C0,
+                    10.051,10.801,3.262,24.125,3.262
+                    S48.25,10.051,48.25,18.425c0,6.453-6.412,11.964-15.45,
+                    14.153"/>
+            </g>
+            <g>
+                <path id = "recurTriangle"
+                d="M 8 0 L 0 4 L 8 8 z"/>
+            </g>
+        </defs>
+    </svg>'''
+
+
 class ModelGraphDisplay(object):
     def __init__(self, data):
         self.data = data
 
     def _ipython_display_(self):
-        js = pkgutil.get_data('nengo.vis', 'static/js/main.js')
-        d3 = pkgutil.get_data('nengo.vis', 'static/js/vendor/d3.min.js')
-        css = pkgutil.get_data('nengo.vis', 'static/css/graph.css')
+        js = get_main_js()
+        d3 = get_d3_js()
+        css = get_svg_css()
         display(HTML('''
             <script>
                 if (typeof(d3) == 'undefined') {{
@@ -35,40 +82,11 @@ class ModelGraphDisplay(object):
             <style type="text/css">{css}</style>
             <script>
                 {js}
-                main({data});
+                ModelVis.init({data});
             </script>
-            <div id="#graph"><svg>
-                <defs>
-                    // the arrow on the links
-                    <marker id="TriangleMarker"
-                            viewBox="0 0 10 10" refX="0" refY="5"
-                            markerUnits="strokeWidth"
-                            markerWidth="6" markerHeight="4"
-                            orient="auto">
-                        <path d="M 0 0 L 10 5 L 0 10 z" />
-                    </marker>
-                    <g id ="ensemble" transform="translate(-17.2735,-17.7015)">
-                        <circle id="mainCircle" cx="16" cy="18" r="18"/>
-                        <circle cx="4.843" cy="10.519" r="4.843"/>
-                        <circle cx="16.186" cy="17.873" r="4.843"/>
-                        <circle cx="21.012" cy="30.56" r="4.843"/>
-                        <circle cx="29.704" cy="17.229" r="4.843"/>
-                        <circle cx="5.647" cy="26.413" r="4.843"/>
-                        <circle cx="19.894" cy="4.842" r="4.843"/>
-                    </g>
-                    <g id = "recur">
-                        <path
-                            d="M6.451,28.748C2.448,26.041,0,22.413,0,18.425C0,
-                            10.051,10.801,3.262,24.125,3.262
-                            S48.25,10.051,48.25,18.425c0,6.453-6.412,11.964-15.45,14.153"/>
-                    </g>
-                    <g>
-                        <path id = "recurTriangle"
-                        d="M 8 0 L 0 4 L 8 8 z"/>
-                    </g>
-                </defs>
-            </svg></div>'''.format(
-                js=js, d3=d3, css=css, data=self.data)))
+            <div id="#graph">{svg}</div>
+        '''.format(
+                js=js, d3=d3, css=css, data=self.data, svg=SVG_TEMPLATE)))
 
     def save(self, filename='modelgraph.svg'):
         display(HTML('''
