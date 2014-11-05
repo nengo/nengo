f3d8d81ea44555cbd55c25c7b37494985bf2aaea
Jan Gosmann <jan@hyper-world.de>
2014-11-05 15:34:40 -0500

    Add unit test for AutoProgressBar.

--- nengo/utils/tests/test_progress.py
+++ nengo/utils/tests/test_progress.py
@@ -3,7 +3,7 @@ import time
 import pytest
 
 import nengo
-from nengo.utils.progress import Progress, ProgressObserver
+from nengo.utils.progress import AutoProgressBar, Progress, ProgressObserver
 
 
 class ProgressObserverMock(ProgressObserver):
@@ -60,6 +60,33 @@ class TestProgress(object):
             assert p.eta == -1  # no estimate available yet
             p.step()
             assert p.eta > 0.
+
+
+class TestAutoProgressBar(object):
+    class ProgressMock(object):
+        def __init__(self, eta, start_time=1234.5):
+            self.eta = eta
+            self.start_time = start_time
+
+    def test_progress_not_shown_if_eta_below_threshold(self):
+        progress_mock = self.ProgressMock(0.2)
+        observer = ProgressObserverMock()
+        auto_progress = AutoProgressBar(observer, min_eta=10.)
+
+        for _ in range(10):
+            auto_progress.update(progress_mock)
+
+        assert observer.n_update_calls == 0
+
+    def test_progress_shown_if_eta_above_threshold(self):
+        progress_mock = self.ProgressMock(20.)
+        observer = ProgressObserverMock()
+        auto_progress = AutoProgressBar(observer, min_eta=10.)
+
+        for _ in range(10):
+            auto_progress.update(progress_mock)
+
+        assert observer.n_update_calls >= 10
 
 
 if __name__ == "__main__":
