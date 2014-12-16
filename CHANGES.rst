6f139a3c1cd5823afefc49ed1896977abd259ab2
Trevor Bekolay <tbekolay@gmail.com>
2014-12-16 14:21:38 -0500

    Add changelog entry for v2.0.0
    And add changelog entries, in general, to the long description
    that will be uploaded to PyPI.

---
+++ CHANGES.rst
@@ -0,0 +1,27 @@
+***************
+Release History
+***************
+
+.. Changelog entries should follow this format:
+
+   version (release date)
+   ======================
+
+   ** section **
+
+   - One-line description of change (link to Github issue/PR)
+
+.. Changes should be organized in one of several sections:
+
+   - API Changes
+   - Improvements
+   - Behavioural Changes
+   - Bugfixes
+   - Documentation
+
+2.0.0 (unreleased)
+==================
+
+Initial release of Nengo 2.0!
+Supports Python 2.6+ and 3.3+.
+Thanks to all of the contributors for making this possible!
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import imp
+import io
 import sys
 import os
 
@@ -49,23 +50,32 @@ class Tox(TestCommand):
         errno = tox.cmdline(args=shlex.split(self.tox_args))
         sys.exit(errno)
 
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
 root = os.path.dirname(os.path.realpath(__file__))
 version_module = imp.load_source(
     'version', os.path.join(root, 'nengo', 'version.py'))
 description = ("Tools for making neural simulations using the methods "
                + "of the Neural Engineering Framework")
-with open(os.path.join(root, 'README.rst')) as readme:
-    long_description = readme.read()
+long_description = read('README.rst', 'CHANGES.rst')
 
 setup(
     name="nengo",
     version=version_module.version,
-    author="CNRGlab at UWaterloo",
+    author="Applied Brain Research",
     author_email="celiasmith@uwaterloo.ca",
     packages=find_packages(),
     scripts=[],
     data_files=[('nengo', ['nengo-data/nengorc'])],
-    url="https://github.com/ctn-waterloo/nengo",
+    url="https://github.com/nengo/nengo",
     license="See LICENSE.rst",
     description=description,
     long_description=long_description,
