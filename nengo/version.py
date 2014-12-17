113822cdc9729640c05942072701600ee267f386
Trevor Bekolay <tbekolay@gmail.com>
2014-12-17 14:27:36 -0500

    Continuing development of 2.0.0

--- nengo/version.py
+++ nengo/version.py
@@ -18,8 +18,8 @@ the version after 2.0.0-rc1 should be 2.0.0-dev, **not** 2.0.0-rc2-dev.
 
 name = "nengo"
 version_info = (2, 0, 0)  # (major, minor, patch)
-rc = 1
-dev = False
+rc = None
+dev = True
 
 version = "{0}{1}{2}".format('.'.join(str(v) for v in version_info),
                              '-rc{0:d}'.format(rc) if rc is not None else '',
