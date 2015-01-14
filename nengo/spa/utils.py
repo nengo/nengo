f9da122d7f1ec92bb8c3ac5be3dd88b94d62f2c6
Eric Hunsberger <erichuns@gmail.com>
2015-01-14 12:10:16 -0500

    Improved nengo.spa.similarity function
    It can now compute the cosine similarity by normalizing.

--- nengo/spa/utils.py
+++ nengo/spa/utils.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 import nengo
-from nengo.spa.vocab import Vocabulary, VocabularyParam
+import nengo.utils.numpy as npext
 from nengo.utils.compat import is_iterable
 
 
@@ -15,13 +15,19 @@ def enable_spa_params(model):
     model : Network
         Model to activate SPA specific parameters for.
     """
+    from nengo.spa.vocab import VocabularyParam
+
     for obj_type in [nengo.Node, nengo.Ensemble]:
         model.config[obj_type].set_param(
             'vocab', VocabularyParam(None, optional=True))
 
 
-def similarity(data, vocab):
+def similarity(data, vocab, normalize=False):
     """Return the similarity between some data and the vocabulary.
+
+    Computes the dot products between all data vectors and each
+    vocabulary vector. If `normalize=True`, normalizes all vectors
+    to compute the cosine similarity.
 
     Parameters
     ----------
@@ -30,13 +36,29 @@ def similarity(data, vocab):
     vocab: spa.Vocabulary, array_like
         Vocabulary (or list of vectors) to use to calculate
         the similarity values
-
+    normalize : boolean (optional)
+        Whether to normalize all vectors, to compute the cosine similarity.
     """
-    if isinstance(vocab, Vocabulary):
-        probe_vectors = vocab.vectors.T
-    elif is_iterable(vocab):
-        probe_vectors = np.matrix(vocab).T
-    else:
-        probe_vectors = vocab.T
+    from nengo.spa.vocab import Vocabulary
 
-    return np.dot(data, probe_vectors)
+    if isinstance(vocab, Vocabulary):
+        vectors = vocab.vectors
+    elif is_iterable(vocab):
+        vectors = np.array(vocab, copy=False, ndmin=2)
+    else:
+        raise ValueError("'%s' object is not a valid vocabulary"
+                         % (vocab.__class__.__name__))
+
+    data = np.array(data, copy=False, ndmin=2)
+    dots = np.dot(data, vectors.T)
+
+    if normalize:
+        # Zero-norm vectors should return zero, so avoid divide-by-zero error
+        eps = np.nextafter(0, 1)  # smallest float above zero
+        dnorm = np.maximum(npext.norm(data, axis=1, keepdims=True), eps)
+        vnorm = np.maximum(npext.norm(vectors, axis=1, keepdims=True), eps)
+
+        dots /= dnorm
+        dots /= vnorm.T
+
+    return dots
