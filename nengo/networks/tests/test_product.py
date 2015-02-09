027135dff6c66be07ac2ce49c4e9d3397e86e6d4
Jan Gosmann <jgosmann@uwaterloo.ca>
2015-02-09 16:01:25 -0500

    Add product network benchmark

--- nengo/networks/tests/test_product.py
+++ nengo/networks/tests/test_product.py
@@ -1,8 +1,12 @@
+from __future__ import print_function
+
 import numpy as np
+import pytest
 
 import nengo
 from nengo.utils.compat import range
-from nengo.utils.numpy import rmse
+from nengo.utils.functions import HilbertCurve
+from nengo.utils.numpy import rmse, maxint
 
 
 def test_sine_waves(Simulator, plt, seed):
@@ -70,3 +74,64 @@ def test_direct_mode_with_single_neuron(Simulator, plt, seed):
     plt.xlim(right=t[-1])
 
     assert rmse(AB[:len(offset), :], sim.data[p][offset, :]) < 0.2
+
+
+@pytest.mark.benchmark
+@pytest.mark.slow
+def test_product_benchmark(analytics, rng):
+    n_trials = 50
+    hc = HilbertCurve(n=4)  # Increase n to cover the input space more densely
+    duration = 5.           # Simulation duration (s)
+    # Duration (s) to wait at the beginning to have a stable representation
+    wait_duration = 0.5
+    n_neurons = 100
+    n_eval_points = 1000
+
+    def stimulus_fn(t):
+        return np.squeeze(hc(t / duration).T * 2 - 1)
+
+    def run_trial():
+        model = nengo.Network(seed=rng.randint(maxint))
+        with model:
+            model.config[nengo.Ensemble].n_eval_points = n_eval_points
+
+            stimulus = nengo.Node(
+                output=lambda t: stimulus_fn(max(0., t - wait_duration)),
+                size_out=2)
+
+            product_net = nengo.networks.Product(n_neurons, 1)
+            nengo.Connection(stimulus[0], product_net.A)
+            nengo.Connection(stimulus[1], product_net.B)
+            probe_test = nengo.Probe(product_net.output)
+
+            ens_direct = nengo.Ensemble(
+                1, dimensions=2, neuron_type=nengo.Direct())
+            result_direct = nengo.Node(size_in=1)
+            nengo.Connection(stimulus, ens_direct)
+            nengo.Connection(
+                ens_direct, result_direct, function=lambda x: x[0] * x[1],
+                synapse=None)
+            probe_direct = nengo.Probe(result_direct)
+
+        sim = nengo.Simulator(model)
+        sim.run(duration + wait_duration, progress_bar=False)
+
+        selection = sim.trange() > wait_duration
+        test = sim.data[probe_test][selection]
+        direct = sim.data[probe_direct][selection]
+        return rmse(test, direct)
+
+    error_data = [run_trial() for i in range(n_trials)]
+    analytics.add_data(
+        'error', error_data, "Multiplication RMSE. Shape: n_trials")
+
+
+@pytest.mark.compare
+def test_compare_product_benchmark(analytics_data):
+    stats = pytest.importorskip('scipy.stats')
+    data1, data2 = (d['error'] for d in analytics_data)
+    improvement = np.mean(data1) - np.mean(data2)
+    p = np.ceil(1000. * 2. * stats.mannwhitneyu(data1, data2)[1]) / 1000.
+    print("Multiplication improvement by {0:f} ({1:.0f}%, p < {2:.3f})".format(
+        improvement, (1. - np.mean(data2) / np.mean(data1)) * 100., p))
+    assert improvement >= 0. or p >= 0.05
