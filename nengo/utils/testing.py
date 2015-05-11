bfa6b049b74ca734b0647335cc15883b47775e87
Jan Gosmann <jan@hyper-world.de>
2015-05-11 17:34:13 -0400

    Always warn in unit tests in case they run twice.
    Fixes #703.

--- nengo/utils/testing.py
+++ nengo/utils/testing.py
@@ -219,6 +219,7 @@ class WarningCatcher(object):
     def __enter__(self):
         self.catcher = warnings.catch_warnings(record=True)
         self.record = self.catcher.__enter__()
+        warnings.simplefilter('always', append=True)
 
     def __exit__(self, type, value, traceback):
         self.catcher.__exit__(type, value, traceback)
