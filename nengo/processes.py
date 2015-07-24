cc13d831fc676de3fe16747eee4fc48024d27190
Eric Hunsberger <erichuns@gmail.com>
2015-07-24 15:48:44 -0400

    Added PresentInput process
    This process presents a series of data, such as images,
    where each data item is presented for the same fixed length of time.

--- nengo/processes.py
+++ nengo/processes.py
@@ -5,7 +5,7 @@ import numpy as np
 import nengo.utils.numpy as npext
 from nengo.dists import DistributionParam, Gaussian
 from nengo.params import (
-    BoolParam, IntParam, NumberParam, Parameter, FrozenObject)
+    BoolParam, IntParam, NdarrayParam, NumberParam, Parameter, FrozenObject)
 from nengo.synapses import LinearFilter, LinearFilterParam, Lowpass
 from nengo.utils.compat import range
 
@@ -262,6 +262,40 @@ class WhiteSignal(Process):
             return signal[i % signal.shape[0]]
 
         return step
+
+
+class PresentInput(Process):
+    """Present a series of inputs, each for the same fixed length of time.
+
+    Parameters
+    ----------
+    inputs : array_like
+        Inputs to present, where each row is an input. Rows will be flattened.
+    presentation_time : float
+        Show each input for `presentation_time` seconds.
+    """
+    inputs = NdarrayParam(shape=('...',))
+    presentation_time = NumberParam(low=0, low_open=True)
+
+    def __init__(self, inputs, presentation_time):
+        self.inputs = inputs
+        self.presentation_time = presentation_time
+        super(PresentInput, self).__init__(
+            default_size_out=self.inputs[0].size)
+
+    def make_step(self, size_in, size_out, dt, rng):
+        assert size_in == 0
+        assert size_out == self.inputs[0].size
+
+        n = len(self.inputs)
+        inputs = self.inputs.reshape(n, -1)
+        presentation_time = float(self.presentation_time)
+
+        def step_image_input(t):
+            i = int(t / presentation_time + 1e-7)
+            return inputs[i % n]
+
+        return step_image_input
 
 
 class ProcessParam(Parameter):
