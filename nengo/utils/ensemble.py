cf8c2900cf833e198123ad006e2dac397930ec42
Eric Hunsberger <erichuns@gmail.com>
2015-10-07 16:53:54 -0400

    FIXUP: function order more alphabetical

--- nengo/utils/ensemble.py
+++ nengo/utils/ensemble.py
@@ -136,78 +136,6 @@ def _similarity(encoders, index, rows, cols=1):
     return sim / count
 
 
-def tune_ens_parameters(ens, function=None, solver=None, rng=None, n=1000):
-    """Find good ensemble parameters for decoding a particular function.
-
-    Randomly generate many sets of parameters and determine the decoding error
-    for each. Then set the ensemble parameters to those with the lowest
-    decoding error. The "ensemble parameters" are the encoders, gains, biases,
-    and evaluation points.
-
-    Parameters
-    ----------
-    ens : Ensemble
-        The ensemble to optimize.
-    function : callable, optional
-        The target function to optimize for. Defaults to the identity function.
-    solver : nengo.solvers.Solver, optional
-        The solver to use for finding the decoders. Default: ``LstsqL2()``
-    rng : numpy.random.RandomState, optional
-        The random number generator to use. Default: ``np.random``
-    n : int, optional
-        The number of random combinations to test. Default: 1000
-    """
-    from nengo.dists import Distribution
-    from nengo.neurons import Direct
-    from nengo.solvers import LstsqL2
-    from nengo.builder.connection import solve_for_decoders
-    from nengo.builder.ensemble import gen_eval_points
-
-    if solver is None:
-        solver = LstsqL2()
-    if rng is None:
-        rng = np.random
-    if isinstance(ens.neuron_type, Direct):
-        raise ValueError("Parameters do not apply to Direct mode ensembles")
-
-    sample = lambda dist, n, d=None: (
-        dist.sample(n, d=d, rng=rng) if isinstance(dist, Distribution)
-        else np.asarray(dist))
-
-    # use the same evaluation points for all trials
-    eval_points = gen_eval_points(ens, ens.eval_points, rng=rng)
-    targets = (np.array([function(ep) for ep in eval_points])
-               if function is not None else eval_points)
-
-    # --- try random parameters and record error
-    errors = []
-    for i in range(n):
-        # --- generate random parameters
-        if ens.gain is None and ens.bias is None:
-            max_rates = sample(ens.max_rates, ens.n_neurons)
-            intercepts = sample(ens.intercepts, ens.n_neurons)
-            gain, bias = ens.neuron_type.gain_bias(max_rates, intercepts)
-        elif ens.gain is not None and ens.bias is not None:
-            gain = sample(ens.gain, ens.n_neurons)
-            bias = sample(ens.bias, ens.n_neurons)
-        else:
-            raise NotImplementedError("Mixed gain/bias and rates/ints")
-
-        encoders = sample(ens.encoders, ens.n_neurons, ens.dimensions)
-
-        # --- determine residual
-        x = np.dot(eval_points, encoders.T / ens.radius)
-        decoders, info = solve_for_decoders(
-            solver, ens.neuron_type, gain, bias, x, targets, rng)
-        error = info['rmses'].mean()
-
-        errors.append((error, encoders, gain, bias, eval_points))
-
-    # --- set parameters to those with the lowest error
-    errors.sort(key=lambda x: x[0])
-    ens.encoders, ens.gain, ens.bias, ens.eval_points = errors[0][1:]
-
-
 def sorted_neurons(ensemble, sim, iterations=100, seed=None):
     """Sort neurons in an ensemble by encoder and intercept.
 
@@ -279,3 +207,75 @@ def sorted_neurons(ensemble, sim, iterations=100, seed=None):
                     indices[[i, j]] = indices[[j, i]]
 
     return indices
+
+
+def tune_ens_parameters(ens, function=None, solver=None, rng=None, n=1000):
+    """Find good ensemble parameters for decoding a particular function.
+
+    Randomly generate many sets of parameters and determine the decoding error
+    for each. Then set the ensemble parameters to those with the lowest
+    decoding error. The "ensemble parameters" are the encoders, gains, biases,
+    and evaluation points.
+
+    Parameters
+    ----------
+    ens : Ensemble
+        The ensemble to optimize.
+    function : callable, optional
+        The target function to optimize for. Defaults to the identity function.
+    solver : nengo.solvers.Solver, optional
+        The solver to use for finding the decoders. Default: ``LstsqL2()``
+    rng : numpy.random.RandomState, optional
+        The random number generator to use. Default: ``np.random``
+    n : int, optional
+        The number of random combinations to test. Default: 1000
+    """
+    from nengo.dists import Distribution
+    from nengo.neurons import Direct
+    from nengo.solvers import LstsqL2
+    from nengo.builder.connection import solve_for_decoders
+    from nengo.builder.ensemble import gen_eval_points
+
+    if solver is None:
+        solver = LstsqL2()
+    if rng is None:
+        rng = np.random
+    if isinstance(ens.neuron_type, Direct):
+        raise ValueError("Parameters do not apply to Direct mode ensembles")
+
+    sample = lambda dist, n, d=None: (
+        dist.sample(n, d=d, rng=rng) if isinstance(dist, Distribution)
+        else np.asarray(dist))
+
+    # use the same evaluation points for all trials
+    eval_points = gen_eval_points(ens, ens.eval_points, rng=rng)
+    targets = (np.array([function(ep) for ep in eval_points])
+               if function is not None else eval_points)
+
+    # --- try random parameters and record error
+    errors = []
+    for i in range(n):
+        # --- generate random parameters
+        if ens.gain is None and ens.bias is None:
+            max_rates = sample(ens.max_rates, ens.n_neurons)
+            intercepts = sample(ens.intercepts, ens.n_neurons)
+            gain, bias = ens.neuron_type.gain_bias(max_rates, intercepts)
+        elif ens.gain is not None and ens.bias is not None:
+            gain = sample(ens.gain, ens.n_neurons)
+            bias = sample(ens.bias, ens.n_neurons)
+        else:
+            raise NotImplementedError("Mixed gain/bias and rates/ints")
+
+        encoders = sample(ens.encoders, ens.n_neurons, ens.dimensions)
+
+        # --- determine residual
+        x = np.dot(eval_points, encoders.T / ens.radius)
+        decoders, info = solve_for_decoders(
+            solver, ens.neuron_type, gain, bias, x, targets, rng)
+        error = info['rmses'].mean()
+
+        errors.append((error, encoders, gain, bias, eval_points))
+
+    # --- set parameters to those with the lowest error
+    errors.sort(key=lambda x: x[0])
+    ens.encoders, ens.gain, ens.bias, ens.eval_points = errors[0][1:]
