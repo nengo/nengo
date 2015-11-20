4fd3a1d2939a106a1c07fa869b6d5848b1dffbce
Terry Stewart <tcstewar@uwaterloo.ca>
2015-11-20 00:29:23 -0500

    Initial reworked version of function space utils

--- nengo/utils/function_space.py
+++ nengo/utils/function_space.py
@@ -1,150 +1,75 @@
-from __future__ import absolute_import
-
 import nengo
 import numpy as np
 
 
-class Function(nengo.dists.Distribution):
-    def __init__(self, function, superimpose=1, **distributions):
-        self.function = function
-        self.distributions = distributions
-        self.superimpose = superimpose
-        super(Function, self).__init__()
-
-    def sample(self, n, d=None, rng=np.random):
-
-        n_samples = n * self.superimpose
-
-        kwargs = {}
-        for k, v in self.distributions.items():
-            if isinstance(v, nengo.dists.Distribution):
-                kwargs[k] = v.sample(n_samples, rng=rng)
-            else:
-                kwargs[k] = np.tile(v, n_samples)
-        values = []
-        index = 0
-        for i in range(n):
-            total = []
-            for j in range(self.superimpose):
-                args = {k:v[index] for k, v in kwargs.items()}
-                total.append(self.function(**args))
-                index +=1
-            values.append(np.sum(total, axis=0))
-        return np.vstack(values)
-
-    def make_stimulus_node(self, function_dist):
-        """Generate a Node that can control function parameters."""
-        def stimulus(t, x):
-            return self.project(self.function(*x))
-
-        return nengo.Node(stimulus, size_in=self.n_params,
-                          size_out=self.n_basis)
-
-class FunctionSpaceDistribution(nengo.dists.Distribution):
-    def __init__(self, function_space, data):
-        self.fs = function_space
-        self.data = data
-        super(FunctionSpaceDistribution, self).__init__()
-
-    def sample(self, n, d=None, rng=np.random):
-        if isinstance(self.data, nengo.dists.Distribution):
-            data = self.data.sample(n, rng=rng)
-        else:
-            data = self.data
-        return np.dot(data, self.fs.basis)
-
-class Combined(nengo.dists.Distribution):
-    def __init__(self, distributions, dimensions,
-                 weights=None, normalize_weights=True):
-        if weights is None:
-            weights = np.ones(len(distributions))
-        if normalize_weights:
-            weights = weights / np.linalg.norm(weights)
-
-        self.weights = weights
-        self.distributions = distributions
-        self.dimensions = dimensions
-        self.n_dimensions = sum(dimensions)
-        super(Combined, self).__init__()
-
-    def sample(self, n, d=None, rng=np.random):
-        assert d == self.n_dimensions
-
-        data = [dist.sample(n, d=self.dimensions[i], rng=rng) * self.weights[i]
-                for i, dist in enumerate(self.distributions)]
-
-        return np.hstack(data)
-
-
-
-
 class FunctionSpace(object):
 
-    def __init__(self, space, n_basis, n_samples=1000, seed=None):
+    def __init__(self, function, n_samples, n_basis, seed=None, pts=None,
+                 **distributions):
         self.rng = np.random.RandomState(seed=seed)
-        self.space = space
+        self.function = function
+        self.pts = pts
+        self.size_output = len(self.call(**distributions))
+        self.n_params = len(distributions)
+
         self.n_basis = n_basis
-        self.n_samples = n_samples
-
-        # these are all computed by self.compute_basis()
-        self._basis = None
-        self._scale = None
-        self._S = None
-
-
-    @property
-    def basis(self):
-        if self._basis is None:
-            self.compute_basis()
-        return self._basis
-
-    @property
-    def scale(self):
-        if self._scale is None:
-            self.compute_basis()
-        return self._scale
-
-    @property
-    def S(self):
-        if self._S is None:
-            self.compute_basis()
-        return self._S
-
-    def compute_basis(self):
-        if isinstance(self.space, nengo.dists.Distribution):
-            data = self.space.sample(self.n_samples, rng=self.rng)
-        else:
-            data = self.space
-
-        U, S, V = np.linalg.svd(data)
-
-        proj = np.dot(data, V[:self.n_basis].T)
-        self._scale = np.mean(np.linalg.norm(proj, axis=1))**2
-        #self._scale = (np.mean(S[:self.n_basis])) ** 2
-        #self._scale = (S[0] / self.n_basis) ** 2
-        #self._scale = np.linalg.norm(S[:self.n_basis])**2
+        data = self.sample(n_samples=n_samples, **distributions)
+        self.U, self.S, self.V = np.linalg.svd(data)
+        #self.scale = (np.mean(self.S[:self.n_basis])) ** 2
+        self.scale = (self.S[0] / self.n_basis) ** 2
         #TODO: which of those scalings works better?
-        self._basis = V[:self.n_basis].T / np.sqrt(self.scale)
-        self._S = S
+        self.basis = self.V[:self.n_basis].T / np.sqrt(self.scale)
+
+    def call(self, function=None, **distributions):
+        """Return one sampled instance of the function"""
+        if function is None:
+            function = self.function
+        kwargs = {}
+        for k, v in distributions.items():
+            if isinstance(v, nengo.dists.Distribution):
+                kwargs[k] = v.sample(1, rng=self.rng)[0]
+            else:
+                kwargs[k] = v
+        return function(self.pts, **kwargs)
+
+    def sample(self, function=None, n_samples=1, superimpose=1,
+               **distributions):
+        """Sample the function n_samples times, with possible superposition"""
+        result = np.zeros((n_samples, self.size_output))
+        for i in range(n_samples * superimpose):
+            result[i / superimpose] += self.call(function, **distributions)
+            #TODO: check if it's faster to sample distributions outside loop
+        return result
 
     def project(self, pts):
-        if isinstance(pts, nengo.dists.Distribution):
-            return FunctionSpaceDistribution(self, pts)
-        else:
-            return np.dot(pts, self.basis)
+        """Encode the function from points into the subspace"""
+        return np.dot(pts, self.basis)
 
     def reconstruct(self, x):
         """Decode the function from the subspace back to points"""
         return np.dot(x, self.basis.T) * self.scale
 
-    def make_plot_node(self, domain, lines=1, n_pts=20, function=None,
+    def set_encoders(self, ens, function=None, **distributions):
+        """Set ensemble encoders by sampling the function"""
+        ens.encoders = self.project(self.sample(function=function,
+                                                n_samples=ens.n_neurons,
+                                                **distributions))
+
+    def set_eval_points(self, ens, function=None, n_eval_points=100,
+                        **distributions):
+        """Set ensemble eval points by sampling the function"""
+        ens.eval_points = self.project(self.sample(function=function,
+                                                   n_samples=n_eval_points,
+                                                   **distributions))
+
+    def make_plot_node(self, lines=1, n_pts=20, function=None,
                        max_x=None, min_x=None, max_y=1, min_y=-1):
         """Generate a Node with a custom GUI plot"""
-        pts = domain
-        indices = None
-        plot_slice = slice(None)
+        basis = self.basis
+        pts = self.pts
         if len(pts) > n_pts:
             indices = np.linspace(0, len(pts) - 1, n_pts).astype(int)
+            basis = basis[indices]
             pts = pts[indices]
 
         if max_x is None:
@@ -158,9 +83,6 @@ class FunctionSpace(object):
                   "#cd79a7", "#f0e542", "#56b4ea"]
 
         def plot_func(t, x):
-            basis = self.basis
-            if indices is not None:
-                basis = basis[indices]
             paths = []
             for i in range(lines):
                 value = x[i*self.n_basis:(i+1)*self.n_basis]
@@ -177,12 +99,10 @@ class FunctionSpace(object):
             plot_func._nengo_html_ = '''
             <svg width="100%%" height="100%%" viewbox="0 0 100 100">%s</svg>
             ''' % (''.join(paths))
-        plot_func._nengo_html_ = ''
-        return nengo.Node(plot_func, size_in=self.n_basis * lines, size_out=0)
+        return nengo.Node(plot_func, size_in=self.n_basis * lines)
 
-    def make_stimulus_node(self, function, n_params):
+    def make_stimulus_node(self):
         """Generate a Node that can control function parameters."""
         def stimulus(t, x):
-            return self.project(function(*x))
-        return nengo.Node(stimulus, size_in=n_params,
-                          size_out=self.n_basis)
+            return fs.project(self.function(fs.pts, *x[1:]) * x[0])
+        return nengo.Node(stimulus, size_in=self.n_params + 1)
