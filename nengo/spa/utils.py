eb00990325bffa5268f9a63cab4999cd038d5a9d
Terry Stewart <tcstewar@uwaterloo.ca>
2016-04-05 16:16:14 +0200

    Updated parameter definition to new syntax
    The definition was accidentally updated incorrectly so that
    the parameter was not set to `None` by default.

--- nengo/spa/utils.py
+++ nengo/spa/utils.py
@@ -20,7 +20,7 @@ def enable_spa_params(model):
 
     for obj_type in [nengo.Node, nengo.Ensemble]:
         model.config[obj_type].set_param(
-            'vocab', VocabularyParam('vocab', optional=True))
+            'vocab', VocabularyParam('vocab', None, optional=True))
 
 
 def similarity(data, vocab, normalize=False):
