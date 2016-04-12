3802f5f495e8de506cfa93cd1adb369173c25fe0
Jan Gosmann <jgosmann@uwaterloo.ca>
2016-04-12 11:56:01 -0400

    Improve syntax for hierarchical SPA inputs.

--- nengo/spa/input.py
+++ nengo/spa/input.py
@@ -12,6 +12,18 @@ def make_parse_func(func, vocab):
     return parse_func
 
 
+class _HierachicalInputProxy(object):
+    def __init__(self, parent, name):
+        self.__dict__['parent'] = parent
+        self.__dict__['name'] = name
+
+    def __getattr__(self, name):
+        return _HierachicalInputProxy(self.parent, self.name + '.' + name)
+
+    def __setattr__(self, name, value):
+        setattr(self.parent, self.name + '.' + name, value)
+
+
 class Input(Module):
     """A SPA module for providing external inputs to other modules.
 
@@ -32,23 +44,40 @@ class Input(Module):
 
     def __init__(self, label=None, seed=None, add_to_container=None, **kwargs):
         super(Input, self).__init__(label, seed, add_to_container)
+        self.spa = None
         self.kwargs = kwargs
         self.input_nodes = {}
+        self._initialized = True
 
     def on_add(self, spa):
         """Create the connections and nodes."""
+        self.spa = spa
         Module.on_add(self, spa)
 
         for name, value in iteritems(self.kwargs):
-            target, vocab = spa.get_module_input(name)
-            if callable(value):
-                val = make_parse_func(value, vocab)
-            else:
-                val = vocab.parse(value).v
+            self.__connect(name, value)
 
-            with self:
-                node = nengo.Node(val, label='input_%s' % name)
-            self.input_nodes[name] = node
+    def __connect(self, name, expr):
+        target, vocab = self.spa.get_module_input(name)
+        if callable(expr):
+            val = make_parse_func(expr, vocab)
+        else:
+            val = vocab.parse(expr).v
 
-            with spa:
-                nengo.Connection(node, target, synapse=None)
+        with self:
+            node = nengo.Node(val, label='input_%s' % name)
+        self.input_nodes[name] = node
+
+        with self.spa:
+            nengo.Connection(node, target, synapse=None)
+
+    def __setattr__(self, name, value):
+        if not getattr(self, '_initialized') or name in self.__dict__:
+            super(Input, self).__setattr__(name, value)
+        else:
+            self.__connect(name, value)
+
+    def __getattr__(self, name):
+        if name == '_initialized':
+            return self.__dict__.get('_initialized', False)
+        return _HierachicalInputProxy(self, name)
