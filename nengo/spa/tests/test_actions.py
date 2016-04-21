9e938abd3c392b23b57b1d87a27c74d00f34e802
Jan Gosmann <jgosmann@uwaterloo.ca>
2016-04-21 15:58:55 -0400

    Add alternate syntax to construct actions.

---
+++ nengo/spa/tests/test_actions.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+import nengo
+from nengo import spa
+
+
+def test_new_action_syntax(Simulator, seed, plt, rng):
+    D = 3
+    model = spa.Module(seed=seed)
+    with model:
+        model.ctrl = spa.Buffer(16, label='ctrl')
+
+        def input_func(t):
+            if t < 0.2:
+                return 'A'
+            elif t < 0.4:
+                return 'B'
+            else:
+                return 'C'
+        model.input = spa.Input(ctrl=input_func)
+
+        model.state = spa.State(D, label='state')
+        model.buff1 = spa.State(D, label='buff1')
+        model.buff2 = spa.State(D, label='buff2')
+        model.buff3 = spa.State(D, label='buff3')
+
+        node1 = nengo.Node([0, 1, 0])
+        node2 = nengo.Node([0, 0, 1])
+
+        nengo.Connection(node1, model.buff1.input)
+        nengo.Connection(node2, model.buff2.input)
+
+        actions = spa.Actions(
+            'state = buff1',
+            'dot(ctrl, A) --> buff3=buff1',
+            'dot(ctrl, B) --> buff3=buff2',
+            'dot(ctrl, C) --> buff3=buff1*buff2')
+        actions.build(model)
+
+        state_probe = nengo.Probe(model.state.output, synapse=0.03)
+        buff3_probe = nengo.Probe(model.buff3.output, synapse=0.03)
+
+    with Simulator(model) as sim:
+        sim.run(0.6)
+
+    data = sim.data[buff3_probe]
+
+    plt.plot(sim.trange(), data)
+
+    state_val = np.mean(sim.data[state_probe][200:], axis=0)
+    assert state_val[0] < 0.2
+    assert state_val[1] > 0.8
+    assert state_val[2] < 0.2
+
+    valueA = np.mean(data[150:200], axis=0)  # should be [0, 1, 0]
+    valueB = np.mean(data[350:400], axis=0)  # should be [0, 0, 1]
+    valueC = np.mean(data[550:600], axis=0)  # should be [1, 0, 0]
+
+    assert valueA[0] < 0.2
+    assert valueA[1] > 0.8
+    assert valueA[2] < 0.2
+
+    assert valueB[0] < 0.2
+    assert valueB[1] < 0.2
+    assert valueB[2] > 0.8
+
+    assert valueC[0] > 0.8
+    assert valueC[1] < 0.2
+    assert valueC[2] < 0.2
