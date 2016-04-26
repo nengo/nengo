85af11aaa0817ab9638ed2b9b60541b5823d5f9a
Trevor Bekolay <tbekolay@gmail.com>
2016-04-26 12:34:13 -0400

    Conform version to PEP440
    PEP440 (https://www.python.org/dev/peps/pep-0440/) defines a standard
    for Python versions that setuptools adheres to. It doesn't allow
    hyphens for marking as a dev version, so we switch to using a dot
    as the standard requires.
    
    The main benefit here is to avoid a warning given by setuptools
    when running `setup.py` on a dev version.

--- nengo/version.py
+++ nengo/version.py
@@ -1,14 +1,14 @@
 """Nengo version information.
 
 We use semantic versioning (see http://semver.org/).
-Additionally, '-dev' will be added to the version unless the code base
-represents a release version. Commits for which the version doesn't have
-'-dev' should be git tagged with the version.
+and confrom to PEP440 (see https://www.python.org/dev/peps/pep-0440/).
+'.devN' will be added to the version unless the code base represents
+a release version. Release versions are git tagged with the version.
 """
 
 name = "nengo"
 version_info = (2, 1, 0)  # (major, minor, patch)
-dev = True
+dev = 0
 
 version = "{v}{dev}".format(v='.'.join(str(v) for v in version_info),
-                            dev='-dev' if dev else '')
+                            dev=('.dev%d' % dev) if dev is not None else '')
