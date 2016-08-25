28a8e27a66768f0d4f5b76137de98c4f385e3df7
Trevor Bekolay <tbekolay@gmail.com>
2016-08-25 13:21:07 -0400

    Fix styling in rendered example notebooks
    The whitespace rule for <code> blocks was causing newlines in
    Markdown cell code listings to disappear, which looked bad.
    This was most apparent in the usage/config example.

--- docs/conf.py
+++ docs/conf.py
@@ -3,15 +3,16 @@
 # This file is execfile()d with the current directory set
 # to its containing dir.
 
+import os
 import sys
 
 try:
     import nengo
     import sphinx_rtd_theme
 except ImportError:
-    print ("To build the documentation, nengo and sphinx_rtd_theme must be "
-           "installed in the current environment. Please install these and "
-           "their requirements first. A virtualenv is recommended!")
+    print("To build the documentation, nengo and sphinx_rtd_theme must be "
+          "installed in the current environment. Please install these and "
+          "their requirements first. A virtualenv is recommended!")
     sys.exit(1)
 
 extensions = [
@@ -62,7 +63,10 @@ pygments_style = 'default'
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_title = "Nengo {0} docs".format(release)
-# html_static_path = ['_static']
+html_static_path = ['_static']
+html_context = {
+    'css_files': [os.path.join('_static', 'custom.css')],
+}
 html_use_smartypants = True
 htmlhelp_basename = 'Nengodoc'
 html_last_updated_fmt = ''  # Suppress 'Last updated on:' timestamp
