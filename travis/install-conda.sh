57dfcf51e0af02cfd56e87395a320d6b8f68c338
Sean Aubin <saubin@uwaterloo.ca>
2016-11-30 20:01:04 +0900

    fixup! second attempt

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -2,16 +2,18 @@
 set -e
 
 # the miniconda directory may exist if it has been restored from cache
-if [ -d "$HOME/miniconda" ] && [ -e "$HOME/miniconda/bin/conda" ]; then
+if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/miniconda/bin/conda" ]; then
     echo "Miniconda install already present from cache: $HOME/miniconda"
 else # if it does not exist, we need to install miniconda
-    rm -rf "$HOME/miniconda" # remove the directory in case we have an empty cached directory
+    rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
     
-  wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
-  bash miniconda.sh -b -p "$HOME/miniconda"
-  export PATH="$HOME/miniconda/bin:$PATH"
-  conda config --set always_yes yes --set changeps1 no
-  conda update -q conda
-  conda info -a
-  conda create -q -n test python="$PYTHON" pip
+    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -p "$MINICONDA_DIR"
+    chown -R "$USER" "$MINICONDA_DIR"
+    export PATH="$HOME/miniconda/bin:$PATH"
+    
+    conda config --set always_yes yes --set changeps1 no
+    conda update -q conda
+    conda info -a
+    conda create -q -n test python="$PYTHON" pip
 fi
