306aca1acbb3b9ee878ba29264e17d2e3c98895d
Sean Aubin <saubin@uwaterloo.ca>
2016-11-30 20:05:44 +0900

    fixup! maybe this will work

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -3,15 +3,15 @@ set -e
 
 # the miniconda directory may exist if it has been restored from cache
 if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/miniconda/bin/conda" ]; then
-    echo "Miniconda install already present from cache: $HOME/miniconda"
+    echo "Miniconda install already present from cache: $MINICONDA_DIR"
 else # if it does not exist, we need to install miniconda
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
     
     wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
-    bash miniconda.sh -b -p "$MINICONDA_DIR"
     chown -R "$USER" "$MINICONDA_DIR"
+    bash miniconda.sh -b -p "$MINICONDA_DIR"
     export PATH="$HOME/miniconda/bin:$PATH"
-    
+
     conda config --set always_yes yes --set changeps1 no
     conda update -q conda
     conda info -a
