9bf6bab4a775fb2dfeed6444ff07f93128c9bea2
Sean Aubin <saubin@uwaterloo.ca>
2016-11-30 20:15:30 +0900

    fixup! fixed command order

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -8,8 +8,8 @@ else # if it does not exist, we need to install miniconda
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
     
     wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
-    chown -R "$USER" "$MINICONDA_DIR"
     bash miniconda.sh -b -p "$MINICONDA_DIR"
+    chown -R "$USER" "$MINICONDA_DIR"
     export PATH="$HOME/miniconda/bin:$PATH"
 
     conda config --set always_yes yes --set changeps1 no
