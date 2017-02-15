6a264c6669821a5c971c0c0c38c6549790b11bf4
Trevor Bekolay <tbekolay@gmail.com>
2017-02-15 13:57:22 -0500

    Add Python 3.6, NumPy 1.12 TravisCI builds
    I don't think there's any reason to have 3.6 with coverage,
    and 3.6 with SciPy with coverage (just the latter will do).
    
    Also switched the order of the static checks since they show up
    in TravisCI in that order.

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
 )
