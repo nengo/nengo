8e12ef7653f455ced4c6e1468f1017ed359921b5
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-03-23 15:47:28 -0400

    Add documentation.

--- nengo/spa/assoc_mem.py
+++ nengo/spa/assoc_mem.py
@@ -1,3 +1,8 @@
+"""Associative memory implementations.
+
+See :doc:`examples/associative_memory` for an introduction and examples.
+"""
+
 import nengo
 import numpy as np
 from nengo.spa.module import Module
@@ -7,6 +12,43 @@ from nengo.utils.network import with_self
 
 
 class AssociativeMemory(Module):
+    """General associative memory module.
+
+    This provides a low-level selection network with the necessary interface
+    to include it within the SPA system.
+
+    Parameters
+    ----------
+    selection_net : Network
+        The network that is used to select the response. It needs to accept
+        the arguments *n_neurons* (number of neurons to use to represent each
+        possible choice) and *n_ensembles* (number of choices). The returned
+        network needs to have an *input* attribute to which the utilities for
+        each choice are connected and an *output* attribute from which a
+        connection will be created to read the selected output(s).
+    input_vocab: list or Vocabulary
+        The vocabulary (or list of vectors) to match.
+    output_vocab: list or Vocabulary, optional (Default: None)
+        The vocabulary (or list of vectors) to be produced for each match. If
+        None, the associative memory will act like an autoassociative memory
+        (cleanup memory).
+    input_keys : list, optional (Default: None)
+        A list of strings that correspond to the input vectors.
+    output_keys : list, optional (Default: None)
+        A list of strings that correspond to the output vectors.
+    n_neurons : int
+        Number of neurons to represent each choice, passed on to the
+        *selection_net*.
+    label : str, optional (Default: None)
+        A name for the ensemble. Used for debugging and visualization.
+    seed : int, optional (Default: None)
+        The seed used for random number generation.
+    add_to_container : bool, optional (Default: None)
+        Determines if this Network will be added to the current container.
+        If None, will be true if currently with
+    vocabs : VocabularyMap, optional (Default: None)
+        Maps dimensionalities to the corresponding default vocabularies.
+    """
     input_vocab = VocabularyOrDimParam(
         'input_vocab', default=None, readonly=True)
     output_vocab = VocabularyOrDimParam(
@@ -58,6 +100,18 @@ class AssociativeMemory(Module):
 
     @with_self
     def add_default_output(self, key, min_activation_value, n_neurons=50):
+        """Adds a Semantic Pointer to output when no other pointer is active.
+
+        Parameters
+        ----------
+        key : str
+            Semantic Pointer to output.
+        min_activation_value : float
+            Minimum output of another Semantic Pointer to deactivate the
+            default output.
+        n_neurons : int, optional (Default: 50)
+            Number of neurons used to represent the default Semantic Pointer.
+        """
         assert not hasattr(self, 'default_ens'), \
             "Can add default output only once."
 
@@ -76,6 +130,10 @@ class AssociativeMemory(Module):
 
 
 class IaAssocMem(AssociativeMemory):
+    """Associative memory based on the `IA` network.
+
+    See `AssociativeMemory` and `IA` for more information.
+    """
     def __init__(
             self, input_vocab, output_vocab=None, input_keys=None,
             output_keys=None, n_neurons=50, label=None, seed=None,
@@ -92,6 +150,10 @@ class IaAssocMem(AssociativeMemory):
 
 
 class ThresholdingAssocMem(AssociativeMemory):
+    """Associative memory based on `ThresholdingArray`.
+
+    See `AssociativeMemory` and `ThresholdingArray` for more information.
+    """
     def __init__(
             self, threshold, input_vocab, output_vocab=None, input_keys=None,
             output_keys=None, n_neurons=50, label=None, seed=None,
@@ -107,6 +169,10 @@ class ThresholdingAssocMem(AssociativeMemory):
 
 
 class WtaAssocMem(AssociativeMemory):
+    """Associative memory based on the `WTA` network.
+
+    See `AssociativeMemory` and `WTA` for more information.
+    """
     def __init__(
             self, threshold, input_vocab, output_vocab=None, input_keys=None,
             output_keys=None, n_neurons=50, label=None, seed=None,
--- nengo/spa/selection.py
+++ nengo/spa/selection.py
@@ -1,4 +1,4 @@
-"""Selection networks."""
+"""Selection networks that pick one or more options among multiple choices."""
 
 import nengo
 import numpy as np
@@ -9,6 +9,64 @@ def IA(
         accum_timescale=0.1, feedback_timescale=0.005,
         accum_synapse=0.1, ff_synapse=0.005,
         intercept_width=0.15, radius=1., **kwargs):
+    """Independent accumulator (IA) winner-take-all (WTA) network.
+
+    This is a two-layered network. The first layer consists of independent
+    accumulators (integrators), whereas the second layer does a thresholding.
+    Once the threshold is exceeded a feedback connection will stabilize the
+    current choice and inhibit all other choices. To switch the selection,
+    it is necessary to provide a transient input to *input_reset* to reset
+    the accumulator states.
+
+    This network is suited especially for accumulating evidence under noisy
+    conditions and keep a stable choice selection until the processing of the
+    choice has been finished.
+
+    Further details are to be found in "A Spiking Independent Accumulator Model
+    for Winner-Take-All Computation" (J Gosmann, AR Voelker, and C Eliasimth;
+    submitted to CogSci 2017).
+
+    Parameters
+    ----------
+    n_neurons : int
+        Number of neurons for each choice.
+    n_ensembles : int
+        Number of choices.
+    accum_threshold : float, optional (Default: 0.8)
+        Accumulation threshold that needs to be reached to produce an output.
+    accum_neuron_ratio: float, optional (Default: 0.7)
+        Portion of *n_neurons* that will be used for a layer 1 accumulator
+        ensemble. The remaining neurons will be used for a layer 2 thresholding
+        ensemble.
+    accum_timescale : float, optional (Default: 0.1)
+        Evidence accumulation timescale.
+    feedback_timescale : float, optional (Default: 0.005)
+        Timescale for the feedback connection from the thresholding layer to
+        the accumulation layer.
+    accum_synapse : Synapse or float, optional (Default: 0.1)
+        The synapse for connections to the accumulator ensembles.
+    ff_synapse : Synapse or float, optional (Default: 0.005)
+        Synapse for feed-forward connections.
+    intercept_width : float, optional (Default: 0.15)
+        The `presets.ThresholdingEnsembles` *intercept_width* parameter.
+    radius : float, optional (Default: 1.0)
+        The representational radius of the ensembles.
+    kwargs : dict
+        Passed on to `nengo.Network`.
+
+    Attributes
+    ----------
+    input : Node
+        The inputs to the network.
+    input_reset : Node
+        Input to reset the accumulators.
+    output : Node
+        The outputs of the network.
+    accumulators : ThresholdingArray
+        The layer 1 accumulators.
+    thresholding : ThresholdingArray
+        The layer 2 thresholding ensembles.
+    """
     n_accum_neurons = int(accum_neuron_ratio * n_neurons)
     n_thresholding_neurons = n_neurons - n_accum_neurons
 
@@ -49,6 +107,38 @@ def IA(
 def ThresholdingArray(
         n_neurons, n_ensembles, threshold, intercept_width=0.15, function=None,
         radius=1., **kwargs):
+    """Array of thresholding ensembles.
+
+    All inputs below the threshold will produce an output of 0, whereas inputs
+    above the threshold produce an output of equal value.
+
+    Parameters
+    ----------
+    n_neurons : int
+        Number of neurons for each ensemble.
+    n_ensembles : int
+        Number of ensembles.
+    threshold : float
+        The thresholding value.
+    intercept_width : float, optional (Default: 0.15)
+        The `presets.ThresholdingEnsembles` *intercept_width* parameter.
+    function : function, optional (Default: None)
+        Function to apply to the thresholded values.
+    radius : float, optional (Default: 1.0)
+        The representational radius of the ensembles.
+    kwargs : dict
+        Arguments passed on to `nengo.Network`.
+
+    Attributes
+    ----------
+    input : Node
+        The inputs to the network.
+    output : Node
+        The outputs of the network.
+    thresholded : Node
+        The raw thresholded value (before applying *function* or correcting
+        for the shift produced by the thresholding).
+    """
     with nengo.Network(**kwargs) as net:
         with nengo.presets.ThresholdingEnsembles(
                 0., intercept_width, radius=radius):
@@ -71,6 +161,31 @@ def ThresholdingArray(
 
 def WTA(n_neurons, n_ensembles, inhibit_scale=1.0, inhibit_synapse=0.005,
         **kwargs):
+    """Winner-take-all (WTA) network with lateral inhibition.
+
+    Parameters
+    ----------
+    n_neurons : int
+        Number of neurons for each ensemble.
+    n_ensembles : int
+        Number of ensembles.
+    inhibit_scale : float, optional (Default: 1.0)
+        Scaling of the lateral inhibition.
+    inhibit_synapse : Synapse or float, optional (Default: 0.005)
+        Synapse on the recurrent connection for lateral inhibition.
+    kwargs : dict
+        Arguments passed on to `ThresholdingArray`.
+
+    Attributes
+    ----------
+    input : Node
+        The inputs to the network.
+    output : Node
+        The outputs of the network.
+    thresholded : Node
+        The raw thresholded value (before applying *function* or correcting
+        for the shift produced by the thresholding).
+    """
     net = ThresholdingArray(n_neurons, n_ensembles, **kwargs)
     with net:
         nengo.Connection(
