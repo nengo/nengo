67aa36e542af3c3873accfb1bd4ad2fa3fb14381
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-05-03 15:06:19 -0400

    Handle parameter equality check for non-arrays.
    Otherwise classes with an __eq__ function could be treated as unequal
    even though they should be equal.

--- nengo/params.py
+++ nengo/params.py
@@ -6,7 +6,7 @@ import numpy as np
 from nengo.exceptions import (
     ConfigError, ObsoleteError, ReadonlyError, ValidationError)
 from nengo.utils.compat import (
-    is_array, is_integer, is_number, is_string, itervalues)
+    is_array, is_array_like, is_integer, is_number, is_string, itervalues)
 from nengo.utils.numpy import array_hash, compare
 from nengo.utils.stdlib import WeakKeyIDDictionary, checked_call
 
@@ -144,8 +144,10 @@ class Parameter(object):
         a = self.__get__(instance_a, None)
         b = self.__get__(instance_b, None)
         if self.equatable:
-            # always use array_equal, in case one argument is an array
-            return np.array_equal(a, b)
+            if is_array_like(a) or is_array_like(b):
+                return np.array_equal(a, b)
+            else:
+                return a == b
         else:
             return a is b
 
