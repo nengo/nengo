a90df2a3c32f29742473e90bfe497a400837bbed
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-01-22 17:13:53 -0500

    fixup! squash! Make sim.n_steps and sim.time scalars

--- nengo/tests/test_simulator.py
+++ nengo/tests/test_simulator.py
@@ -25,8 +25,8 @@ def test_steps(RefSimulator):
         assert sim.n_steps == 2
         assert np.allclose(sim.time, 2 * dt)
 
-        assert np.isscalar(sim.n_steps.shape)
-        assert np.isscalar(sim.time.shape)
+        assert np.isscalar(sim.n_steps)
+        assert np.isscalar(sim.time)
 
 
 def test_time_absolute(Simulator):
