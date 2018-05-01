06872dcb1b438824944abf6dfb4c07f24eccb84e
Trevor Bekolay <tbekolay@gmail.com>
2018-05-01 12:59:40 -0400

    Build docs with -v flag
    This is not really for our own benefit (though it may be
    useful for debugging some issues) it is done to reduce the
    chances that the TravisCI build will fail because there has
    been no output in the past 10 minutes.

--- .ci/docs.sh
+++ .ci/docs.sh
@@ -17,7 +17,7 @@ if [[ "$COMMAND" == "install" ]]; then
     pip install "ipython[all]==2.4.1" sphinx guzzle_sphinx_theme ghp-import
 elif [[ "$COMMAND" == "run" ]]; then
     rm "$HOME/.ipython/profile_default/ipython_config.py"
-    sphinx-build -W docs docs/_build
+    sphinx-build -vW docs docs/_build
 elif [[ "$COMMAND" == "upload" ]]; then
     DATE=$(date '+%Y-%m-%d %T')
     git config --global user.email "travis@travis-ci.org"
