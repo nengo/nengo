fd5066c39bc684cda022f8a722dcecd225e8174c
Trevor Bekolay <tbekolay@gmail.com>
2018-05-23 13:39:41 -0400

    Check example style during CI

--- .ci/static.sh
+++ .ci/static.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+shopt -s globstar
+
 NAME=$0
 COMMAND=$1
+STATUS=0  # Used to exit with non-zero status if any check fails
 
 function usage {
     echo "usage: $NAME <command>"
@@ -12,9 +15,24 @@ function usage {
 }
 
 if [[ "$COMMAND" == "install" ]]; then
+    conda install jupyter
     pip install codespell flake8 pylint
 elif [[ "$COMMAND" == "run" ]]; then
-    flake8 -v nengo && codespell -q 3 && pylint nengo
+    # Convert notebooks to Python scripts
+    jupyter-nbconvert \
+        --log-level WARN \
+        --to python \
+        --TemplateExporter.exclude_input_prompt=True \
+        -- **/*.ipynb
+    # Remove style issues introduced in the conversion:
+    #   s/# $/#/g replaces lines with just '# ' with '#'
+    #   /get_ipython()/d removes lines containing 'get_ipython()'
+    sed -i -e 's/# $/#/g' -e '/get_ipython()/d' -- docs/**/*.py
+    flake8 nengo || STATUS=1
+    flake8 --ignore=E703,W391 docs || STATUS=1
+    pylint docs nengo || STATUS=1
+    rm docs/examples/**/*.py
+    codespell -q 3 --skip="./build,./docs/_build,*-checkpoint.ipynb"|| STATUS=1
 else
     if [[ -z "$COMMAND" ]]; then
         echo "Command required"
@@ -24,3 +42,4 @@ else
     echo
     usage
 fi
+exit $STATUS
