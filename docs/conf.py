c8b82e0ee248584cff8e543772209d68f8e0e51a
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2019-11-11 14:56:07 -0400

    Update for Bones 0.7.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,7 @@ import nengo
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
     "sphinx.ext.githubpages",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
@@ -25,6 +26,11 @@ extensions = [
 autoclass_content = "both"  # class and __init__ docstrings are concatenated
 autodoc_default_options = {"members": None}
 autodoc_member_order = "bysource"  # default is alphabetical
+
+# -- sphinx.ext.doctest
+doctest_global_setup = """
+import nengo
+"""
 
 # -- sphinx.ext.intersphinx
 intersphinx_mapping = {
